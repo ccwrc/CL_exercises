a18b7084bbe40fc4c0cd30f08839ffb79cf76271
ccwrc <j.muszelik@gmail.com>
2017-01-06 13:09:57 +0100

    in progress

---
+++ PHP_podstawy/4.Tablice/exercise_1.php
@@ -0,0 +1,14 @@
+<?php
+
+/* Napisz funkcję printTable($array) która w pętli przejdzie po wszystkich elementach tablicy i 
+ * wypisze je na ekranie. */
+
+$testArray = ['123w', 'edw11', '1qa11', 'wew22', 'www', 'aa222', 'qq', '909'];
+
+
+function printTable($testArray) {
+    foreach ($testArray as $value) {
+        echo $value . "<br>";
+    }
+}
+
