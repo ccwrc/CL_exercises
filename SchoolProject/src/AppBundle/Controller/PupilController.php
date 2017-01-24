096154e2ac6d72df439b1c300950e20287b863d2
ccwrc <j.muszelik@gmail.com>
2017-01-24 17:18:16 +0100

    school project - dump database

--- SchoolProject/src/AppBundle/Controller/PupilController.php
+++ SchoolProject/src/AppBundle/Controller/PupilController.php
@@ -6,76 +6,75 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
-
+// odstep miedzy pakietami standardowymi a wlasnymi wskazany/zalecany
 use AppBundle\Entity\Pupil;
 
-class PupilController extends Controller
-{
-   /**
-    * @Route("/pupil/new")
-    * @Method("GET")
-    */ 
-   public function newPupilAction() {
-       return $this->render("AppBundle:Pupil:new.html.twig");
-   }
-   
+class PupilController extends Controller {
+
     /**
-    * @Route("/pupil/new")
-    * @Method("POST")
-    */ 
-   public function createPupilAction(Request $req) {
-       $em = $this->getDoctrine()->getManager();
-       
-       $name = $req->request->get("name");
-       $age = $req->request->get("age");
-       $description = $req->request->get("description");
-       
-       $newPupil = new Pupil();
-       $newPupil->setAge($age);
-       $newPupil->setDescription($description);
-       $newPupil->setName($name);
-       
-       $em->persist($newPupil);
-       $em->flush();
-       
-       //TODO: redirect do pokazania jednego ucznia (na todo jest wyszukiwanie :)))
-       return $this->redirectToRoute("app_pupil_showpupil", ["id" => $newPupil->getId()]);
-   }
-   
-      
+     * @Route("/pupil/new")
+     * @Method("GET")
+     */
+    public function newPupilAction() {
+        return $this->render("AppBundle:Pupil:new.html.twig");
+    }
+
     /**
-    * @Route("/pupil/{id}", requirements={"id"="\d+"})
-    * @Method("GET")
-    */ 
-   public function showPupilAction($id) {
-       $repo = $this->getDoctrine()->getRepository('AppBundle:Pupil');
-       $pupil = $repo->find($id);
-       
-       return $this->render("AppBundle:Pupil:show.html.twig", [
-           "pupil" => $pupil]);
-   }
-   
-   /**
-    * @Route("/getpupilswithoutclass")
-    */
-   
-   public function getpupilswithoutclass() {
-       $repo = $this->getDoctrine()->getRepository('AppBundle:Pupil');
-       $pwc = $repo->getAllPupilsWithoutClass();
-       
-       return $this->render("AppBundle:Pupil:list.html.twig", [
-           "pupils" => $pwc]);
-   }
-   
+     * @Route("/pupil/new")
+     * @Method("POST")
+     */
+    public function createPupilAction(Request $req) {
+        $em = $this->getDoctrine()->getManager();
+
+        $name = $req->request->get("name");
+        $age = $req->request->get("age");
+        $description = $req->request->get("description");
+
+        $newPupil = new Pupil();
+        $newPupil->setAge($age);
+        $newPupil->setDescription($description);
+        $newPupil->setName($name);
+
+        $em->persist($newPupil); //daje znac, ze istnieje
+        $em->flush(); // dane leca zbiorczo, akurat tu jedna sztuka 
+
+        //TODO: redirect do pokazania jednego ucznia (na todo jest wyszukiwanie :)))
+        // no i todo zrobione ponizej...
+        return $this->redirectToRoute("app_pupil_showpupil", ["id" => $newPupil->getId()]);
+    }
+
     /**
-    * @Route("/getpupilswithBetween/{max}/{min}")
-    */
-   public function getpupilswithBetween($max, $min) {
-       $repo = $this->getDoctrine()->getRepository('AppBundle:Pupil');
-       $pwc = $repo->getAllPupilsWithAgeBetween($min, $max);
-       
-       return $this->render("AppBundle:Pupil:list.html.twig", [
-           "pupils" => $pwc]);
-   }
-    
+     * @Route("/pupil/{id}", requirements={"id"="\d+"})
+     * @Method("GET")
+     */
+    public function showPupilAction($id) {
+        $repo = $this->getDoctrine()->getRepository('AppBundle:Pupil');
+        $pupil = $repo->find($id);
+
+        return $this->render("AppBundle:Pupil:show.html.twig", [
+                    "pupil" => $pupil]);
+    }
+
+    /**
+     * @Route("/getpupilswithoutclass")
+     */
+    public function getpupilswithoutclass() {
+        $repo = $this->getDoctrine()->getRepository('AppBundle:Pupil');
+        $pwc = $repo->getAllPupilsWithoutClass();
+
+        return $this->render("AppBundle:Pupil:list.html.twig", [
+                    "pupils" => $pwc]);
+    }
+
+    /**
+     * @Route("/getpupilswithBetween/{max}/{min}")
+     */
+    public function getpupilswithBetween($max, $min) {
+        $repo = $this->getDoctrine()->getRepository('AppBundle:Pupil');
+        $pwc = $repo->getAllPupilsWithAgeBetween($min, $max);
+
+        return $this->render("AppBundle:Pupil:list.html.twig", [
+                    "pupils" => $pwc]);
+    }
+
 }
