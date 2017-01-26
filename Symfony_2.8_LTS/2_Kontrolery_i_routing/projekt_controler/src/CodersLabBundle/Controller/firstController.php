e1fdfd7e0f4f5846628502e290aee1eb26351336
ccwrc <j.muszelik@gmail.com>
2017-01-26 22:52:02 +0100

    symfony in progress

--- Symfony_2.8_LTS/2_Kontrolery_i_routing/projekt_controler/src/CodersLabBundle/Controller/firstController.php
+++ Symfony_2.8_LTS/2_Kontrolery_i_routing/projekt_controler/src/CodersLabBundle/Controller/firstController.php
@@ -5,13 +5,61 @@ namespace CodersLabBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
 
-class firstController extends Controller
-{
+/**
+ * @Route("/first")
+ */
+class firstController extends Controller {
+
     /**
      * @Route("/helloWorld/")
      */
     public function helloWorldAction() {
         return new Response("<h1>hello...</h1>");
     }
+    
+    /**
+     * @Route("/goodbye/{username}")
+     */
+    public function goodbyeAction($username) {
+        return new Response("Goodbye " . $username);
+    }
+    
+    /**
+     * @Route("/welcome/{name}/{surname}",defaults={"name"="Funky","surname"="Koval"})
+     */
+    public function welcomeAction($name, $surname) { 
+        return new Response("Welcome " . $name . " " . $surname);
+    }
+    
+    /**
+     * @Route("/showPost/{id}", requirements={"id"="\d+"})
+     */
+    public function showPostAction($id) {
+        return new Response("Showing post " . $id);
+    }
+    
+    /**
+     * @Route("/form")
+     * @Method("GET")
+     */
+    public function getFormAction() {
+        $html = "<form method='POST'><input action='' type='text' name='postem"
+                . "'/><input type='submit'/></form>";
+        return new Response($html);
+    }
+    
+    /**
+     * @Route("/form")
+     * @Method("POST")
+     */
+    public function postFormAction(Request $req) {
+        $text = $req->request->get("postem");
+        return new Response($text);
+    }
+    
+
+
 }
