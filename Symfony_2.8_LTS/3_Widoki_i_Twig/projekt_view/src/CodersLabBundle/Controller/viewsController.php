5760b59032b450d1765798e95f957e055f21e8c8
ccwrc <j.muszelik@gmail.com>
2017-01-28 15:57:55 +0100

    symfony 3b1 - done

--- Symfony_2.8_LTS/3_Widoki_i_Twig/projekt_view/src/CodersLabBundle/Controller/viewsController.php
+++ Symfony_2.8_LTS/3_Widoki_i_Twig/projekt_view/src/CodersLabBundle/Controller/viewsController.php
@@ -5,6 +5,26 @@ namespace CodersLabBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class viewsController extends Controller
-{
+class viewsController extends Controller {
+    
+    /**
+     * @Route("/render")
+     */
+    public function renderAction() {
+        return $this->render("CodersLabBundle:view:view_ex_a3.html.twig");
+    }
+    
+    /**
+     * @Route("/render/{username}")
+     */
+    public function renderUsernameAction($username) {
+        return $this->render("CodersLabBundle:view:view_ex_b1.html.twig", [
+            "username" => $username
+        ]);
+    }
+
+
+    /* Stwórz nową akcję przypisaną do adresu /render/{username}. Podepnij do niej widok 
+     * view_ex_b1.html.twig (który możesz znaleźć w katalogu z zadaniami). 
+     * Widok ten przyjmuje jedną zmienną o nazwie username. Przekaż ją. */
 }
