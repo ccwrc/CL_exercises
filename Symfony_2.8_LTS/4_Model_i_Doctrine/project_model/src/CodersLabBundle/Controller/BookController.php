b03fae6717f850984c8db8295bec0f1ed4ade427
ccwrc <j.muszelik@gmail.com>
2017-01-30 16:29:23 +0100

    symfony 4c1 - done

--- Symfony_2.8_LTS/4_Model_i_Doctrine/project_model/src/CodersLabBundle/Controller/BookController.php
+++ Symfony_2.8_LTS/4_Model_i_Doctrine/project_model/src/CodersLabBundle/Controller/BookController.php
@@ -5,6 +5,17 @@ namespace CodersLabBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class BookController extends Controller
-{
+class BookController extends Controller {
+    
+    /**
+     * @Route("/newBook")
+     */
+    public function newBookAction() {
+        return $this->render("CodersLabBundle:Book:new_book.html.twig");
+    }
+    
+    
+    /* Stwórz akcję /newBook, która ma wyświetlać formularz do tworzenia nowej książki. 
+     * Formularz – póki co – napisz w normalnym HTML. Formularz ma kierować do akcji 
+     * /createBook. */
 }
