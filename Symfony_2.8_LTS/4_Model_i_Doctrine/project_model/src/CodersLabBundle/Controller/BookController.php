1211f6bfba7150b21ec3f8ce73cdf5c823bda08f
ccwrc <j.muszelik@gmail.com>
2017-01-30 17:03:19 +0100

    symfony 4c2 - done

--- Symfony_2.8_LTS/4_Model_i_Doctrine/project_model/src/CodersLabBundle/Controller/BookController.php
+++ Symfony_2.8_LTS/4_Model_i_Doctrine/project_model/src/CodersLabBundle/Controller/BookController.php
@@ -4,6 +4,11 @@ namespace CodersLabBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use CodersLabBundle\Entity\Book;
 
 class BookController extends Controller {
     
@@ -14,8 +19,33 @@ class BookController extends Controller {
         return $this->render("CodersLabBundle:Book:new_book.html.twig");
     }
     
-    
-    /* Stwórz akcję /newBook, która ma wyświetlać formularz do tworzenia nowej książki. 
-     * Formularz – póki co – napisz w normalnym HTML. Formularz ma kierować do akcji 
-     * /createBook. */
+    /**
+     * @Route("/createBook")
+     * @Method("POST")
+     */
+    public function createBookAction(Request $req) {
+        $em = $this->getDoctrine()->getManager();
+        
+        $bookTitle = $req->request->get('title');
+        $bookDescription = $req->request->get('description');
+        $bookRating = $req->request->get('rating');
+        
+        $newBook = new Book();
+        $newBook->setTitle($bookTitle);
+        $newBook->setDescription($bookDescription);
+        $newBook->setRating($bookRating);
+        
+        $em->persist($newBook);
+        $em->flush();
+        
+        return new Response("książka o id: " . $newBook->getId() . " została utworzona");
+    }
+
+
+
+
+    /* Stwórz akcję /createBook. Akcja ta ma pobierać informacje z POST-a i na jej podstawie 
+     * tworzyć i zapamiętywać do bazy danych nową książkę. Na razie powinna wyświetlać 
+     * statyczną informację o tym, że udało się stworzyć książkę (co zweryfikuj za pomocą 
+     * PHPMyAdmin). */
 }
