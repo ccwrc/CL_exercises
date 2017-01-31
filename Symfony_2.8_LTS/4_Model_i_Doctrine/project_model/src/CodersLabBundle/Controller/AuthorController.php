f18680a4c96a8ce1963523bdc5cad3148cd9ff7c
ccwrc <j.muszelik@gmail.com>
2017-01-31 19:47:55 +0100

    symfony 4d1 - done

--- Symfony_2.8_LTS/4_Model_i_Doctrine/project_model/src/CodersLabBundle/Controller/AuthorController.php
+++ Symfony_2.8_LTS/4_Model_i_Doctrine/project_model/src/CodersLabBundle/Controller/AuthorController.php
@@ -4,26 +4,53 @@ namespace CodersLabBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class AuthorController extends Controller
-{
+use CodersLabBundle\Entity\Author;
+
+class AuthorController extends Controller {
+    
     /**
-     * @Route("/createAuthor")
+     * @Route("/newAuthor")
      */
-    public function createAuthorAction() {
-        
-        return $this->render('CodersLabBundle:Author:create_author.html.twig', array(
-            // ...
-        ));
+    public function newAuthorAction() {
+        return $this->render("CodersLabBundle:Author:new_author.html.twig");
     }
 
     /**
-     * @Route("/displayAuthor")
+     * @Route("/createAuthor")
+     * @Method("POST")
      */
-    public function displayAuthorAction() {
+    public function createAuthorAction(Request $req) {
+        $em = $this->getDoctrine()->getManager();
+
+        $authorNameSurname = $req->request->get('nameSurname');
+        $authorDescription = $req->request->get('description');
+
+        $newAuthor = new Author();
+        $newAuthor->setDescription($authorDescription);
+        $newAuthor->setNameSurname($authorNameSurname);
+
+        $em->persist($newAuthor);
+        $em->flush();
+ 
+        return $this->render("CodersLabBundle:Author:display_author.html.twig", [
+                    "id" => $newAuthor->getId(),
+                    "author" => $newAuthor
+        ]);
+    }
+
+    /**
+     * @Route("/displayAuthor/{id}")
+     */
+    public function displayAuthorAction($id) {
+        $authorRepo = $this->getDoctrine()->getRepository("CodersLabBundle:Author");
+        $author = $authorRepo->find($id);
         
         return $this->render('CodersLabBundle:Author:display_author.html.twig', array(
-            // ...
+            "author" => $author
         ));
     }
 
@@ -31,9 +58,11 @@ class AuthorController extends Controller
      * @Route("/showAllAuthors")
      */
     public function showAllAuthorsAction() {
+        $authorRepo = $this->getDoctrine()->getRepository("CodersLabBundle:Author");
+        $authors = $authorRepo->findAll();
         
         return $this->render('CodersLabBundle:Author:show_all_authors.html.twig', array(
-            // ...
+            "authors" => $authors
         ));
     }
 
