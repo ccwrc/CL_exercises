62e6e2e165f16b9e2751680e43994f0f422a5e58
ccwrc <j.muszelik@gmail.com>
2017-01-31 21:13:12 +0100

    symfony 4d2 - done + dump database

--- Symfony_2.8_LTS/4_Model_i_Doctrine/fake_database_dump/31.01.2017book_exercises.sql
+++ Symfony_2.8_LTS/4_Model_i_Doctrine/fake_database_dump/31.01.2017book_exercises.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 31, 2017 at 12:29 PM
+-- Generation Time: Jan 31, 2017 at 09:11 PM
 -- Server version: 5.7.17-0ubuntu0.16.04.1
 -- PHP Version: 7.0.8-0ubuntu0.16.04.3
 
@@ -23,6 +23,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `author`
+--
+
+CREATE TABLE `author` (
+  `id` int(11) NOT NULL,
+  `nameSurname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `author`
+--
+
+INSERT INTO `author` (`id`, `nameSurname`, `description`) VALUES
+(1, 'imię i nazwisko autora', 'opis autora'),
+(2, 'name 2', 'opis 2'),
+(3, 'name 3', 'opis 3'),
+(4, 'nameAutor 4', 'opisAutor 4'),
+(5, 'nameAutor 5', 'opisAutor 5');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `book`
 --
 
@@ -30,39 +53,62 @@ CREATE TABLE `book` (
   `id` int(11) NOT NULL,
   `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `rating` double NOT NULL
+  `rating` double NOT NULL,
+  `author_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `book`
 --
 
-INSERT INTO `book` (`id`, `title`, `description`, `rating`) VALUES
-(2, 'tytuł książki', 'opis książki', 22),
-(3, 'tytuł książki2', 'opis książki2', 221),
-(4, '4 ksiazka', '4 opis książki', 4),
-(5, '5t', 'o5', 5),
-(6, '5t', 'o5', 5);
+INSERT INTO `book` (`id`, `title`, `description`, `rating`, `author_id`) VALUES
+(2, 'tytuł książki', 'opis książki', 22, NULL),
+(3, 'tytuł książki2', 'opis książki2', 221, NULL),
+(4, '4 ksiazka', '4 opis książki', 4, NULL),
+(5, '5t', 'o5', 5, NULL),
+(6, '5t', 'o5', 5, NULL);
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `author`
+--
+ALTER TABLE `author`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `book`
 --
 ALTER TABLE `book`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_CBE5A331F675F31B` (`author_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `author`
+--
+ALTER TABLE `author`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
 -- AUTO_INCREMENT for table `book`
 --
 ALTER TABLE `book`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `book`
+--
+ALTER TABLE `book`
+  ADD CONSTRAINT `FK_CBE5A331F675F31B` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
