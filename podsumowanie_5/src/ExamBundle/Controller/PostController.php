d8f40460e4a44ef9dba4e6cde65219919d576e53
ccwrc <j.muszelik@gmail.com>
2017-02-07 15:36:02 +0100

    show foobar post action - done

--- podsumowanie_5/src/ExamBundle/Controller/PostController.php
+++ podsumowanie_5/src/ExamBundle/Controller/PostController.php
@@ -4,27 +4,39 @@ namespace ExamBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class PostController extends Controller
-{
+use ExamBundle\Entity\Post;
+
+class PostController extends Controller {
+    
     /**
      * @Route("/changeTitlePost")
      */
-    public function changeTitlePostAction()
-    {
-        return $this->render('ExamBundle:Post:change_title_post.html.twig', array(
-            // ...
-        ));
+    public function changeTitlePostAction() {
+        $repo = $this->getDoctrine()->getRepository("ExamBundle:Post");
+        $post = $repo->findOneById(3);
+
+        if (!$post) {
+            return new Response("Brak id w bazie");
+        }
+        $post->setTitle("Some other title");
+        $em = $this->getDoctrine()->getManager();
+        $em->flush();
+
+        return new Response("zmiany zapisane w bazie, tytuł zmieniony");
     }
 
     /**
      * @Route("/showFooBarPosts")
      */
-    public function showFooBarPostsAction()
-    {
-        return $this->render('ExamBundle:Post:show_foo_bar_posts.html.twig', array(
-            // ...
-        ));
+    public function showFooBarPostsAction() {
+        $repo = $this->getDoctrine()->getRepository("ExamBundle:Post");
+        $posts = $repo->findByTitle("fooBar"); //DQL nie uzwglednia wielkosci liter
+        $postCounter = count($posts);
+
+        return new Response("liczba postów to: " . $postCounter);
     }
 
     /**
