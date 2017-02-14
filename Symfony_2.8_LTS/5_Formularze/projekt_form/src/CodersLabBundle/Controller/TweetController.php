2fa60d3b374e4e841c3a0d9f81b897a4e455c945
ccwrc <j.muszelik@gmail.com>
2017-02-14 16:55:30 +0100

    a3 - done

--- Symfony_2.8_LTS/5_Formularze/projekt_form/src/CodersLabBundle/Controller/TweetController.php
+++ Symfony_2.8_LTS/5_Formularze/projekt_form/src/CodersLabBundle/Controller/TweetController.php
@@ -4,28 +4,61 @@ namespace CodersLabBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 use CodersLabBundle\Entity\Tweet;
 
-class TweetController extends Controller
-{
+class TweetController extends Controller {
+    
+    /**
+     * @Route("/")
+     */
+    public function indexAction() {
+        return $this->render('CodersLabBundle:Default:index.html.twig');
+    }
+
     /**
      * @Route("/create")
      */
-    public function createAction()
-    {
+    public function createAction(Request $req) {
+        $tweet = new Tweet();
+        $form = $this->createFormBuilder($tweet)
+                ->setMethod("POST")
+                ->add("name", "text", ["label" => "Podaj nazwę: "])
+                ->add("text", "text", ["label" => "podaj tekst: "])
+                ->add("save", "submit", ["label" => "Zapisz tweeta"])
+                ->getForm();
+
+        $form->handleRequest($req);
+        if ($form->isSubmitted()) {
+            $tweet = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($tweet);
+            $em->flush();
+            return new Response("klawo jak cholera Egon!");
+        }
+
         return $this->render('CodersLabBundle:Tweet:create.html.twig', array(
-            // ...
+                        //
         ));
     }
 
     /**
      * @Route("/new")
      */
-    public function newAction()
-    {
+    public function newAction() {
+        $tweet = new Tweet();
+        $form = $this->createFormBuilder($tweet)
+                ->setAction($this->generateUrl("coderslab_tweet_create"))
+                ->setMethod("POST")
+                ->add("name", "text", ["label" => "Podaj nazwę: "])
+                ->add("text", "text", ["label" => "podaj tekst: "])
+                ->add("save", "submit", ["label" => "Zapisz tweeta"])
+                ->getForm();
+
         return $this->render('CodersLabBundle:Tweet:new.html.twig', array(
-            // ...
+                    "form" => $form->createView()
         ));
     }
 
@@ -40,11 +73,6 @@ class TweetController extends Controller
         ));
     }
     
-    /**
-     * @Route("/")
-     */
-    public function indexAction() {
-        return $this->render('CodersLabBundle:Default:index.html.twig');
-    }
+
 
 }
