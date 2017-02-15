8e39b696bed421aba84f961d6cecfd79a3de7a97
ccwrc <j.muszelik@gmail.com>
2017-02-15 21:45:01 +0100

    a5 - auto crud

---
+++ Symfony_2.8_LTS/6_Walidacja/auto_create_CRUD/project_validation/src/CodersLabBundle/Form/BookType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace CodersLabBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('title')->add('rating')->add('description')->add('pages')->add('author')        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'CodersLabBundle\Entity\Book'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'coderslabbundle_book';
+    }
+
+
+}
