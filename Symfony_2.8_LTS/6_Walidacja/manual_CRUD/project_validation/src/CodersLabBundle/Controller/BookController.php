132e203148735ae87021cf40a3975eb8014e5707
ccwrc <j.muszelik@gmail.com>
2017-02-16 17:29:09 +0100

    a5 - done

--- Symfony_2.8_LTS/6_Walidacja/manual_CRUD/project_validation/src/CodersLabBundle/Controller/BookController.php
+++ Symfony_2.8_LTS/6_Walidacja/manual_CRUD/project_validation/src/CodersLabBundle/Controller/BookController.php
@@ -10,7 +10,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use CodersLabBundle\Entity\Book;
 
 class BookController extends Controller {
-    
+
     /**
      * @Route("/createBook")
      */
@@ -43,12 +43,16 @@ class BookController extends Controller {
     }
 
     /**
-     * @Route("/showBook")
+     * @Route("/showBook/{id}")
      */
-    public function showBookAction()
-    {
+    public function showBookAction($id) {
+        $book = $this->getDoctrine()->getRepository("CodersLabBundle:Book")->find($id);
+        if ($book == null) {
+            throw $this->createNotFoundException("brak id w bazie");
+        }
+
         return $this->render('CodersLabBundle:Book:show_book.html.twig', array(
-            // ...
+                    "book" => $book
         ));
     }
 
@@ -64,23 +68,48 @@ class BookController extends Controller {
     }
 
     /**
-     * @Route("/editBook")
+     * @Route("/editBook/{id}")
      */
-    public function editBookAction()
-    {
+    public function editBookAction(Request $req, $id) {
+        $book = $this->getDoctrine()->getRepository("CodersLabBundle:Book")->find($id);
+        $form = $this->createFormBuilder($book)
+                ->setMethod("POST")
+                ->add("title", "text")
+                ->add("rating", "number")
+                ->add("description", "text")
+                ->add("pages", "number")
+                ->add("author", EntityType::class, [
+                    "class" => "CodersLabBundle:Author", "choice_label" => "name"
+                ])
+                ->add("save", "submit")
+                ->getForm();
+
+        $form->handleRequest($req);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $book = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+            return $this->redirectToRoute("coderslab_book_showallbooks");
+        }
+
         return $this->render('CodersLabBundle:Book:edit_book.html.twig', array(
-            // ...
+                    "form" => $form->createView()
         ));
     }
 
     /**
-     * @Route("/deleteBook")
+     * @Route("/deleteBook/{id}")
      */
-    public function deleteBookAction()
-    {
-        return $this->render('CodersLabBundle:Book:delete_book.html.twig', array(
-            // ...
-        ));
+    public function deleteBookAction($id) {
+        $book = $this->getDoctrine()->getRepository("CodersLabBundle:Book")->find($id);
+        if ($book == null) {
+            throw $this->createNotFoundException("brak id w bazie");
+        }
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($book);
+        $em->flush();
+
+        return $this->redirectToRoute("coderslab_book_showallbooks");
     }
 
 }
