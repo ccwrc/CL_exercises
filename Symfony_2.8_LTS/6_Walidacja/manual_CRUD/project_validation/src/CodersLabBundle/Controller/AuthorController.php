3384c807e4a16426298a45a2fb63bbc0e5f58729
ccwrc <j.muszelik@gmail.com>
2017-02-16 15:59:40 +0100

    create author - done

--- Symfony_2.8_LTS/6_Walidacja/manual_CRUD/project_validation/src/CodersLabBundle/Controller/AuthorController.php
+++ Symfony_2.8_LTS/6_Walidacja/manual_CRUD/project_validation/src/CodersLabBundle/Controller/AuthorController.php
@@ -4,18 +4,36 @@ namespace CodersLabBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 use CodersLabBundle\Entity\Author;
 
-class AuthorController extends Controller
-{
+class AuthorController extends Controller {
+    
     /**
      * @Route("/createAuthor")
      */
-    public function createAuthorAction()
-    {
+    public function createAuthorAction(Request $req) {
+        $author = new Author();
+        $form = $this->createFormBuilder($author)
+                ->setMethod("POST")
+                ->add("name", "text")
+                ->add("description", "text")
+                ->add("age", "integer")
+                ->add("save", "submit")
+                ->getForm();
+
+        $form->handleRequest($req);
+        if ($form->isValid() && $form->isSubmitted()) {
+            $author = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($author);
+            $em->flush();
+            return $this->redirectToRoute("coderslab_author_showallauthors");
+        }
+
         return $this->render('CodersLabBundle:Author:create_author.html.twig', array(
-            // ...
+                    "form" => $form->createView()
         ));
     }
 
@@ -52,13 +70,18 @@ class AuthorController extends Controller
     }
 
     /**
-     * @Route("/deleteAuthor")
+     * @Route("/deleteAuthor/{id}")
      */
-    public function deleteAuthorAction()
-    {
-        return $this->render('CodersLabBundle:Author:delete_author.html.twig', array(
-            // ...
-        ));
+    public function deleteAuthorAction($id) {
+        $author = $this->getDoctrine()->getRepository("CodersLabBundle:Author")->find($id);
+        if ($author == null) {
+            throw $this->createNotFoundException("brak id w bazie");
+        }
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($author);
+        $em->flush();
+
+        return $this->redirectToRoute("coderslab_author_showallauthors");
     }
 
 }
