3444957f472b36bd71367000312531a6086e7bb4
ccwrc <j.muszelik@gmail.com>
2017-03-08 21:41:08 +0100

    D4 - create basic pages

---
+++ MySQL/part_1/zadanieD4/index.php
@@ -0,0 +1,8 @@
+<?php
+/* stwórz miniportal, w którym:
+    Możemy dodać/usuwać kina, filmy, bilety i płatności.
+    Możemy wyświetlać wszystkie kina, wyszukiwać je po nazwie.
+    Możemy wyświetlać wszystkie filmy, wyszukiwać je po nazwie lub ratingu.
+    Możemy wyświetlać wszystkie płatności, wyszukiwać je po dacie (uwzględnij 
+ * wyszukiwanie starszych niż/nowszych niż/z danego zakresu/z danej daty). */
+
