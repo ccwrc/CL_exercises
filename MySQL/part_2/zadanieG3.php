b66ffdf1f4595dfb04b48f349d4fddd97f41e76a
ccwrc <j.muszelik@gmail.com>
2017-03-11 16:40:06 +0100

    G3 - add relations

---
+++ MySQL/part_2/zadanieG3.php
@@ -0,0 +1,18 @@
+<?php
+/* Połącz tabele Kina i Filmy poprzez relację wiele do wielu (film może być wyświetlany w wielu 
+ * kinach, kino może mieć wiele filmów). Dodatkowo stworzoną tabelę nazwij Seans. Następnie:
+
+    Napisz stronę, na której można stworzyć nowy seans.
+    Napisz stronę, na której można wybrać wszystkie kina mające w repertuarze dany film.
+    Napisz stronę, na której wypisane są wszystkie filmy wyświetlane w danym kinie.   */
+
+// tworzenie tabeli
+//    CREATE TABLE show_cm(
+//    id int AUTO_INCREMENT NOT NULL,
+//    cinema_id int NOT NULL,
+//    movie_id int NOT NULL,
+//    PRIMARY KEY(id),
+//    FOREIGN KEY(cinema_id) REFERENCES Cinemas(id),
+//    FOREIGN KEY(movie_id) REFERENCES Movies(id)
+//    );
+
