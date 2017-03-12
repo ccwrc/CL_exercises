40aa6d8ec4bcb59437b17ce5f5f6d82453070a23
ccwrc <j.muszelik@gmail.com>
2017-03-12 18:40:40 +0100

    G4 - add_cinema - done

--- MySQL/part_2/zadanieG4/admin_add_cinema.php
+++ MySQL/part_2/zadanieG4/admin_add_cinema.php
@@ -1,8 +1,34 @@
 <?php
+include 'connection.php';
+include 'functions.php';
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$conn = connectToCinemaDb();
 
+if ($conn->connect_error) {
+    die('Błąd połączenia' . $conn->connect_error);
+}
+$setEncodingSql = "SET CHARSET utf8";
+$conn->query($setEncodingSql);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['cinema_name'] != '' 
+        && $_POST['cinema_address'] != '') {
+    $cinemaName = $conn->real_escape_string($_POST['cinema_name']);
+    $cinemaAddress = $conn->real_escape_string($_POST['cinema_address']);
+// mozna dodac sprawdzanie dlugosci i sanityzacje kodu
+    
+    $sql = "INSERT INTO `Cinemas` (`id`, `name`, `adress`) VALUES "
+            . "(NULL, '$cinemaName', '$cinemaAddress')";
+    $result = $conn->query($sql);
+    if ($result) {
+        echo "Kino dodane";
+    } else {
+        echo "błąd dodawania kina";
+    }
+}
+?>
+
+<form method="POST" action="">
+    <input type="text" name="cinema_name" placeholder="podaj nazwę kina"/>
+    <input type="text" name="cinema_address" placeholder="podaj adres kina"/>
+    <input type="submit" value="zapisz"/>
+</form>
