4d16cfa683736a2cc92089665310156c2bf1c7fb
ccwrc <j.muszelik@gmail.com>
2017-03-14 21:51:30 +0100

    ticket_buy - done

--- MySQL/part_2/zadanieG4/ticket_buy.php
+++ MySQL/part_2/zadanieG4/ticket_buy.php
@@ -1,9 +1,25 @@
 <?php
 include 'src/menu.php';
+include 'src/connection.php';
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$conn = connectToCinemaDb();
 
+$sql = "SELECT Cinemas.name AS cinema, Movies.name AS movie, show_cm.id AS show_id FROM Cinemas 
+JOIN show_cm ON Cinemas.id=show_cm.cinema_id
+JOIN Movies ON Movies.id=show_cm.movie_id";
+
+$result = $conn->query($sql);
+?>
+
+<h3>Kup bilet na seans</h3>
+<form method="POST" action="ticket_pay.php">
+    <select name="show_id">
+        <option>wybierz seans z listy</option>
+        <?php
+        foreach ($result as $row) {
+            echo "<option value=" . $row['show_id'] . ">" . $row['cinema'] . " | Film: " . $row['movie'] . "</option>";
+        }
+        ?>
+        <input type="submit" value="kup">
+    </select>
+</form>
