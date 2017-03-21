77caadfb15a35a544576020f4a77c9644dd6838f
ccwrc <j.muszelik@gmail.com>
2017-03-21 21:09:23 +0100

    A2 - done

--- JavaScript/1_Podstawy_JavaScript/1_Podstawy/js/zadanieA2.js
+++ JavaScript/1_Podstawy_JavaScript/1_Podstawy/js/zadanieA2.js
@@ -1,22 +1,49 @@
-/**
- * Created by Jacek on 2016-01-11.
- */
-
-
 function myEval(a, b, operation) {
     var result = 0;
-    switch(operation) {
-        case'+': result = a + b;
-        case'-': result = a - value.b;
-        case'/': result = a / b;
-        case'*': result = a * b;
-        case'%': result = a % b;
-        case'*': result = Math.pow(a, b);
+    switch (operation) {
+        case'+':
+        {
+            result = a + b;
+            break;
+        }
+        case'-':
+        {
+            result = a - b;
+            break;
+        }
+        case'/':
+        {
+            result = a / b;
+            break;
+        }
+        case'*':
+        {
+            result = a * b;
+            break;
+        }
+        case'%':
+        {
+            result = a % b;
+            break;
+        }
+        case'^':
+        {
+            result = Math.pow(a, b);
+            break;
+        }
+        default:
+        {
+            result = "Błędny operator";
+        }
     }
     return result;
 }
 
+//poniżej dopisz pozostałe przypadki działań jakie ma obsługiwać funkcja
 console.log(" 2 + 4 = " + myEval(2, 4, "+"));
 console.log(" 5 - 2 = " + myEval(5, 2, "-"));
+console.log(" 5 / 2 = " + myEval(5, 2, "/"));
+console.log(" 5 * 2 = " + myEval(5, 2, "*"));
+console.log(" 5 % 2 = " + myEval(5, 2, "%"));
 console.log(" 4 ^ 8 = " + myEval(4, 8, "^"));
-//poniżej dopisz pozostałe przypadki działań jakie ma obsługiwać funkcja
+console.log(" 4 bla 8 = " + myEval(4, 8, "bla"));
