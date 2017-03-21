0bb6f7313f1723a0622597f8d84cae8df3a17549
ccwrc <j.muszelik@gmail.com>
2017-03-21 21:41:14 +0100

    js podstawy - done

--- JavaScript/1_Podstawy_JavaScript/1_Podstawy/js/zadanieB3.js
+++ JavaScript/1_Podstawy_JavaScript/1_Podstawy/js/zadanieB3.js
@@ -1,17 +1,22 @@
 //Komentarze do kodu umieśc bezpośrednio pod linią w której znaleziono błąd
 
-var i = 0;
-while (true < 100) {
+var i = 1;
+// nie powinno być 0 tylko 1 (petla nieskonczona)
+while (i < 100) {
+// zamiast true<100 powinno byc i<100 
     console.log('Aktualna wartość zmiennej i to: ' + i);
     i = i * 2;
 }
 
-var year = 2016;
-if(year %= 4) {
+var year = 2017;
+if (year % 4 == 0) {
+// byl blad logiczny i przypisanie zamist porownania
     console.log('Rok przestępny');
-    else
+} else { // nie bylo klamer {}
     console.log('Rok nieprzestępny');
 }
 
 
-console.log('Błędy poprawione, więc ten tekst będę widzieć w konsoli po uruchomieniu skryptu'.' HURRRA!!!');
+
+console.log('Błędy poprawione, więc ten tekst będę widzieć w konsoli po uruchomieniu skryptu' + ' HURRRA!!!');
+// . zamiast +
