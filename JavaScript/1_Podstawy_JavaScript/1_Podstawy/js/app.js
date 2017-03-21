d5c6ed2948253bd4c2af6dc92889824598fe1a0c
ccwrc <j.muszelik@gmail.com>
2017-03-21 21:50:58 +0100

    add b1 in app.js

--- JavaScript/1_Podstawy_JavaScript/1_Podstawy/js/app.js
+++ JavaScript/1_Podstawy_JavaScript/1_Podstawy/js/app.js
@@ -1,5 +1,20 @@
-/**
- * 
- */
+// b1
 
+var liczba = 10;
+console.log(typeof liczba);
 
+var text = "Ala ma kota";
+console.log(typeof text);
+
+var prawda = true;
+console.log(typeof prawda);
+
+var foo = null;
+console.log(typeof foo); // wynik to obiekt
+
+var tab1 = [1, 2, "Ala"];
+console.log(typeof tab1);  // wynik to obiekt
+
+var dodaj = prawda + text;
+console.log(dodaj);
+console.log(typeof dodaj);  //string
