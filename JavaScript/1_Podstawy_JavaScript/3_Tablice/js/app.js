f543ed3e1cd2268dfd2ca9f6af758beb0ec429ad
ccwrc <j.muszelik@gmail.com>
2017-03-22 13:05:50 +0100

    a2 - done

--- JavaScript/1_Podstawy_JavaScript/3_Tablice/js/app.js
+++ JavaScript/1_Podstawy_JavaScript/3_Tablice/js/app.js
@@ -1,3 +1,21 @@
-/**
- * Created by Jacek on 2016-01-11.
- */
+/* Zadanie A2
+Napisz dwie funkcje add(array) i multiply(array). Obie mają przyjmować tylko jeden argument 
+– tablicę. Następnie funkcja add ma zsumować wszystkie liczby znajdujące się w tej 
+tablicy (przy pomocy pętli for), a funkcja multiply ma pomnożyć wszystkie liczby 
+znajdujące się w tablicy (przy pomocy pętli for). */
+
+function multiply(array) {
+    var result = 1;
+    for (var i = 0; i < array.length; i++) {
+        result *= array[i];
+    }
+    return result;
+}
+
+function add(array) {
+    var sumResult = 0;
+    for (var i = 0; i < array.length; i++) {
+        sumResult += array[i];
+    }
+    return sumResult;
+}
