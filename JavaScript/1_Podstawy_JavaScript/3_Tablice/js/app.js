a6b7cb0cc168e142d7b4dad312702c6b12141249
ccwrc <j.muszelik@gmail.com>
2017-03-22 13:15:45 +0100

    a3 - done

--- JavaScript/1_Podstawy_JavaScript/3_Tablice/js/app.js
+++ JavaScript/1_Podstawy_JavaScript/3_Tablice/js/app.js
@@ -19,3 +19,24 @@ function add(array) {
     }
     return sumResult;
 }
+
+/* Zadanie A3
+Napisz funkcję distFromAvarage, która ma przyjmować jako argument tablicę z liczbami. 
+Zwracana przez funkcję tablica ma przedstawiać w kazdej komórce różnicę między liczbą z 
+danej komórki a średnią wartością tablicy. Dla uproszczenia możesz użyć funkcji z poprzedniego 
+zadania. Np.
+
+distFromAvarage([1,2,3,4,5,6,7]) => [3,2,1,0,1,2,3] (średnia z tablicy wejściowej to 4)
+distFromAvarage([1,1,1,1]) => [0,0,0,0] (średnia z tablicy wejściowej to 1)
+distFromAvarage([2,8,3,7]) => [3,3,2,2] (średnia z tablicy wejściowej to 5) */
+
+function distFromAvarage(array) {
+    var avg = add(array) / array.length;
+    console.log(avg);
+    var diff = [];
+    for (var i = 0; i < array.length; i++) {
+        var result = Math.abs(array[i] - avg);
+        diff.push(result);
+    }
+    console.log(diff);
+}
