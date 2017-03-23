19b6d7d3ef0007423455c1ec055befe44ffd8b83
ccwrc <j.muszelik@gmail.com>
2017-03-23 16:24:36 +0100

    zadanie 2 - done

--- JavaScript/1_Podstawy_JavaScript/6_Zaawansowane_funkcje/js/zadanie02.js
+++ JavaScript/1_Podstawy_JavaScript/6_Zaawansowane_funkcje/js/zadanie02.js
@@ -1,32 +1,35 @@
-/**
- * Zagnieżdżanie funkcji.
+/**Zagnieżdżanie funkcji.
  * Prześledź krok po kroku każdą linijkę kodu. Każdą!
  * Spróbuj napisać komentarze dla każdej z linijek kodu
- */
+ * 
+ * Zadanie 2
+Zajrzyj do pliku zadanie02.js i przetestuj działanie obu funkcji. Dlaczego funkcja o nazwie jeden 
+nie ma dostępu do zmiennej o nazwie zmienna2? Aby potwierdzić zrozumienie tematu spróbuj napisać 
+komentarz przy każdej z linii kodu.    */
 
 
-//Twój komentarz ...
+//na koncu deklaracji zabraklo srednika
 function jeden() {
 
-    //Twój komentarz ...
+    //utworzenie i przypisanie zmiennej lokalnej
     var zmienna1 = 1;
 
     //Twój komentarz ...
     function dwa() {
 
-        //Twój komentarz ...
+        //wywolanie zmiennej widocznej dla funkcji dwa, ktora jest podfunkcja f. jeden
         console.log(zmienna1);
 
-        //Twój komentarz ...
+        //utworzenie i przypisanie zmiennej lokalnej (brak widocznosci dla funkcji jeden - nieladnie)
         var zmienna2 = 3;
     }
 
-    //Twój komentarz ...
+    //wywolanie funkcji dwa w funkcji jeden
     dwa();
 
-    //Twój komentarz ...
-    console.log(zmienna2)
-}
+    //brak srednika, brak wczesniejszej deklaracji zmiennej w danym zakresie
+    console.log(zmienna2);
+};
 
-//Twój komentarz ...
-jeden()
+//brak srednika
+jeden();
