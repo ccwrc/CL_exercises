4e3636e41b29275f750a465a7e97612f79ad0cd2
ccwrc <j.muszelik@gmail.com>
2017-03-24 15:59:17 +0100

    1_Wyszukiwanie_elementow - done

--- JavaScript/2_DOM/1_Wyszukiwanie_elementow/js/app.js
+++ JavaScript/2_DOM/1_Wyszukiwanie_elementow/js/app.js
@@ -12,63 +12,74 @@ document.addEventListener("DOMContentLoaded", function () {
      które wyszukują wiele elementów. Za każdym razem wypisz w konsoli, ile jest znalezionych 
      przez Ciebie elementów. */
 
-//    var article = document.querySelector("article.first");
-//    console.log(article);
-//
-//    var searchH2InArticle = article.querySelectorAll("h2");
-//    console.log("ilość h2 w article: " + searchH2InArticle.length);
-//    //alternatywa: article.getElemetsByTagName('h2')
-//
-//    var oferts = article.querySelectorAll(".oferts");
-//    console.log(oferts);
-//    console.log("ilosc znalezionych oferts: " + oferts.length);
-//    for (var i = 0; i < oferts.length; i++) {
-//        console.log(oferts[i]);
-//    }
-//
-//    var divs = article.getElementsByTagName("div");
-//    console.log("ilosc znalezionych divs: " + divs.length);
-//    for (var i = 0; i < divs.length; i++) {
-//        console.log(divs[i]);
-//    }
-    
+    var article = document.querySelector("article.first");
+    console.log(article);
+
+    var searchH2InArticle = article.querySelectorAll("h2");
+    console.log("ilość h2 w article: " + searchH2InArticle.length);
+    //alternatywa: article.getElemetsByTagName('h2')
+
+    var oferts = article.querySelectorAll(".oferts");
+    console.log(oferts);
+    console.log("ilosc znalezionych oferts: " + oferts.length);
+    for (var i = 0; i < oferts.length; i++) {
+        console.log(oferts[i]);
+    }
+
+    var divs = article.getElementsByTagName("div");
+    console.log("ilosc znalezionych divs: " + divs.length);
+    for (var i = 0; i < divs.length; i++) {
+        console.log(divs[i]);
+    }
+
     /* Zadanie 2 
-Wyszukaj na stronie i zapisz do zmiennej element o id exercise2, który znajduje się
- w menu. Skorzystaj z selektora CSS. Nie odwołuj się bezpośredniego do klasy lub id
-  jakiegokolwiek elementu. Użyj selektorów potomków, dzieci, n-tych dzieci itp. */
+     Wyszukaj na stronie i zapisz do zmiennej element o id exercise2, który znajduje się
+     w menu. Skorzystaj z selektora CSS. Nie odwołuj się bezpośredniego do klasy lub id
+     jakiegokolwiek elementu. Użyj selektorów potomków, dzieci, n-tych dzieci itp. */
 
-//    var exer2 = document.querySelector("nav ul li:nth-child(5) a");
-//    console.log("exer2: " + exer2);
+    var exer2 = document.querySelector("nav ul li:nth-child(5) a");
+    console.log("exer2: " + exer2);
 
-/* Zadanie 3
-Wyszukaj na stronie następujące elementy i zapisz je do odpowiednio nazwanych zmiennych:
-    Element o id home (na dwa sposoby).
-    Pierwszy element li nieposiadający atrybutu data-direction.
-    Pierwszy element o klasie block.
-Pamiętaj, żeby za każdym razem sprawdzić, czy wczytałeś odpowiedni element. Używaj 
-funkcji wyszukujących tylko jeden element. */
+    /* Zadanie 3
+     Wyszukaj na stronie następujące elementy i zapisz je do odpowiednio nazwanych zmiennych:
+     Element o id home (na dwa sposoby).
+     Pierwszy element li nieposiadający atrybutu data-direction.
+     Pierwszy element o klasie block.
+     Pamiętaj, żeby za każdym razem sprawdzić, czy wczytałeś odpowiedni element. Używaj 
+     funkcji wyszukujących tylko jeden element. */
 
-var idHome = document.getElementById("home"); // 1 sposob
-var idHome2 = document.querySelector("body header"); // 2 sposob
-var idHome3 = document.querySelector("#home"); // 3 sposob
-var firstLiWithoutData = document.querySelector("nav ul li:not([data-direction])");
-var firstElWithBlockClass = document.querySelector(".block");
+    var idHome = document.getElementById("home"); // 1 sposob
+    var idHome2 = document.querySelector("body header"); // 2 sposob
+    var idHome3 = document.querySelector("#home"); // 3 sposob
+    var firstLiWithoutData = document.querySelector("nav ul li:not([data-direction])");
+    var firstElWithBlockClass = document.querySelector(".block");
 
-/* Zadanie 4
-Wyszukaj na stronie następujące elementy i zapisz je do odpowiednio nazwanych zmiennych:
-    Wszystkie elementy li znajdujące się w tagu nav.
-    Wszystkie paragrafy należące do wszystkich elementów div.
-    Wszystkie divy znajdujące się w tagu article.
-Pamiętaj, żeby za każdym razem sprawdzić, czy wczytałeś odpowiednie elementy. 
-Pamiętaj, żeby używać funkcji, które wyszukują wiele elementów. Za każdym razem 
-wypisz w konsoli, ile znalazłeś elementów. */
+    /* Zadanie 4
+     Wyszukaj na stronie następujące elementy i zapisz je do odpowiednio nazwanych zmiennych:
+     Wszystkie elementy li znajdujące się w tagu nav.
+     Wszystkie paragrafy należące do wszystkich elementów div.
+     Wszystkie divy znajdujące się w tagu article.
+     Pamiętaj, żeby za każdym razem sprawdzić, czy wczytałeś odpowiednie elementy. 
+     Pamiętaj, żeby używać funkcji, które wyszukują wiele elementów. Za każdym razem 
+     wypisz w konsoli, ile znalazłeś elementów. */
 
-var allLiInNavTag = document.querySelectorAll("nav li");
-console.log("allLiInNavTag: " + allLiInNavTag.length);
-var allParagraphsInDivs = document.querySelectorAll("div p");
-console.log("allParagraphsInDivs: " + allParagraphsInDivs.length);
-var allDivsInArticle = document.querySelectorAll("article div");
-console.log("allDivsInArticle: " + allDivsInArticle.length);
+    var allLiInNavTag = document.querySelectorAll("nav li");
+    console.log("allLiInNavTag: " + allLiInNavTag.length);
+    var allParagraphsInDivs = document.querySelectorAll("div p");
+    console.log("allParagraphsInDivs: " + allParagraphsInDivs.length);
+    var allDivsInArticle = document.querySelectorAll("article div");
+    console.log("allDivsInArticle: " + allDivsInArticle.length);
 
+    /* Zadanie 5
+     Znajdź wszystkie elementy li, które są w tagu nav. Następnie nadaj każdemu elementowi
+     li atrybut data-direction = "top", ale tylko dla tych elementów, które nie mają 
+     ustawionego tego atrybutu. */
+
+    var allLiInNavWithoutData = document.querySelectorAll("nav ul li:not([data-direction])");
+    console.log("allLiInNavWithoutData: " + allLiInNavWithoutData.length);
+    
+    for (var i = 0; i < allLiInNavWithoutData.length; i++) {
+        allLiInNavWithoutData[i].setAttribute("data-direction", "top");
+    }
 
 });
