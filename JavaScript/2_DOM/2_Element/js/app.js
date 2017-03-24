cc8098746e3353866d2594e3bf47632b36eb712e
ccwrc <j.muszelik@gmail.com>
2017-03-24 21:32:42 +0100

    2_Element - done

--- JavaScript/2_DOM/2_Element/js/app.js
+++ JavaScript/2_DOM/2_Element/js/app.js
@@ -6,39 +6,37 @@ document.addEventListener("DOMContentLoaded", function () {
     var blocks = document.querySelectorAll(".block");
     var links = document.querySelector(".links").children;
 
-    /* Poniżej napisz kod rozwiązujący zadania */
-
     /* Zadanie 1
      W pliku js dopisz kod, który zamieni miejscami klasę i id dla elementu znajdującego
      się w zmiennej homeElement */
 
-//    var homeElementId = homeElement.getAttribute("id");
-//    var homeElementClass = homeElement.getAttribute("class");
-//
-//    homeElement.setAttribute("class", homeElementId);
-//    homeElement.setAttribute("id", homeElementClass);
+    var homeElementId = homeElement.getAttribute("id");
+    var homeElementClass = homeElement.getAttribute("class");
+
+    homeElement.setAttribute("class", homeElementId);
+    homeElement.setAttribute("id", homeElementClass);
 
     /* Zadanie 2 
      Przeanalizuj kod HTML i JavaScript tego zadania. Wypisz w konsoli wszystkie wcześniej 
      przygotowane zmienne i ich typy. W przypadku tablic przeiteruj (używając np. pętli 
      for), wypisując wartość i typ dla każdego elementu, który znajduje się w tablicy. */
 
-//    function showElement(element) {
-//        if (element.length > 0) {
-//            for (var i = 0; i < element.length; i++) {
-//                console.log(element[i].tagName);
-//                console.log(element[i]);
-//            }
-//        } else {
-//            console.log(element.tagName);
-//            console.log(element);
-//        }
-//    }
-//    showElement(homeElement);
-//    showElement(childElements);
-//    showElement(banner);
-//    showElement(blocks);
-//    showElement(links);
+    function showElement(element) {
+        if (element.length > 0) {
+            for (var i = 0; i < element.length; i++) {
+                console.log(element[i].tagName);
+                console.log(element[i]);
+            }
+        } else {
+            console.log(element.tagName);
+            console.log(element);
+        }
+    }
+    showElement(homeElement);
+    showElement(childElements);
+    showElement(banner);
+    showElement(blocks);
+    showElement(links);
 
     /* Zadanie 3
      Wypisz w konsoli wartości innerHTML i outerHTML dla elementów zmiennej blocks. 
@@ -46,26 +44,43 @@ document.addEventListener("DOMContentLoaded", function () {
      "Nowa wartość diva o klasie blocks". Przeanalizuj, jak zmienił się kod HTML 
      strony. */
 
-//    for (var i = 0; i < blocks.length; i++) {
-//       // po przypisaniu stary kod html zostal zastapiony stringiem
-//        blocks[i].innerHTML = "Nowa wartość diva o klasie blocks";
-//        console.log(blocks[i].innerHTML); // tylko wew. html
-//        console.log(blocks[i].outerHTML); // calosc z zewnetrznym html
-//    }
-    
+    for (var i = 0; i < blocks.length; i++) {
+        // po przypisaniu stary kod html zostal zastapiony stringiem
+        blocks[i].innerHTML = "Nowa wartość diva o klasie blocks";
+        console.log(blocks[i].innerHTML); // tylko wew. html
+        console.log(blocks[i].outerHTML); // calosc z zewnetrznym html
+    }
+
     /* Zadanie 4
-Wypisz w konsoli id elementu kryjącego się w zmiennej homeElement. */
-    
+     Wypisz w konsoli id elementu kryjącego się w zmiennej homeElement. */
+
     console.log(homeElement.getAttribute("id"));
     console.log(homeElement.id); //sposob drugi
-    
+
     /* Zadanie 5
-Wypisz w konsoli tagi wszystkich elementów, które znajdują się w zmiennej
- childElements (pamiętaj o tym, że jest to tablica). */
-    
-    for (var i=0; i < childElements.length; i++) {
+     Wypisz w konsoli tagi wszystkich elementów, które znajdują się w zmiennej
+     childElements (pamiętaj o tym, że jest to tablica). */
+
+    for (var i = 0; i < childElements.length; i++) {
         console.log(childElements[i].tagName);
     }
-    
+
+    /* Zadanie 6
+     Wypisz w konsoli wartość atrybutu dataset dla każdego elementu znajdującego się 
+     w zmiennej links (pamiętaj o tym, że jest to tablica). */
+
+    for (var i = 0; i < links.length; i++) {
+        console.log(links[i].dataset);
+    }
+
+    /* Zadanie 7
+     Wypisz w konsoli wartość atrybutów classList i className dla zmiennej banner.
+     Wypisz w konsoli, jakiego typu (i ewentualnie jakiej klasy) są te wartości. */
+
+    console.log(banner.classList);
+    console.log("typ classList: " + typeof banner.classList);
+
+    console.log(banner.className);
+    console.log("typ className: " + typeof banner.className);
 
 });
