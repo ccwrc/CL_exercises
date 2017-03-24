18779254e43f7294da038cfa14a376e08bedb6f6
ccwrc <j.muszelik@gmail.com>
2017-03-24 21:10:58 +0100

    zadanie 5 - done

--- JavaScript/2_DOM/2_Element/js/app.js
+++ JavaScript/2_DOM/2_Element/js/app.js
@@ -46,17 +46,26 @@ document.addEventListener("DOMContentLoaded", function () {
      "Nowa wartość diva o klasie blocks". Przeanalizuj, jak zmienił się kod HTML 
      strony. */
 
-    for (var i = 0; i < blocks.length; i++) {
-       // po przypisaniu stary kod html zostal zastapiony stringiem
-        blocks[i].innerHTML = "Nowa wartość diva o klasie blocks";
-        console.log(blocks[i].innerHTML); // tylko wew. html
-        console.log(blocks[i].outerHTML); // calosc z zewnetrznym html
-    }
+//    for (var i = 0; i < blocks.length; i++) {
+//       // po przypisaniu stary kod html zostal zastapiony stringiem
+//        blocks[i].innerHTML = "Nowa wartość diva o klasie blocks";
+//        console.log(blocks[i].innerHTML); // tylko wew. html
+//        console.log(blocks[i].outerHTML); // calosc z zewnetrznym html
+//    }
     
     /* Zadanie 4
 Wypisz w konsoli id elementu kryjącego się w zmiennej homeElement. */
     
     console.log(homeElement.getAttribute("id"));
     console.log(homeElement.id); //sposob drugi
+    
+    /* Zadanie 5
+Wypisz w konsoli tagi wszystkich elementów, które znajdują się w zmiennej
+ childElements (pamiętaj o tym, że jest to tablica). */
+    
+    for (var i=0; i < childElements.length; i++) {
+        console.log(childElements[i].tagName);
+    }
+    
 
 });
