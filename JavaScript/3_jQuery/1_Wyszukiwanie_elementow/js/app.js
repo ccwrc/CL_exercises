5d7d9ca9573effd9263ac618ad3e46a516814d1d
ccwrc <j.muszelik@gmail.com>
2017-03-30 17:12:20 +0200

    zadanie 4 - done

--- JavaScript/3_jQuery/1_Wyszukiwanie_elementow/js/app.js
+++ JavaScript/3_jQuery/1_Wyszukiwanie_elementow/js/app.js
@@ -31,11 +31,24 @@ $(document).ready(function () {
 //  $(".main li").fadeOut(3000);
 //  $(".main li").fadeIn(3000);  //ponizej juz z wykorzystaniem each
 
-    $(".main").find("li").each(function () {
-        $(this).addClass("colorText backgroundElement");
-        $(this).fadeOut(2000);
-        $(this).fadeIn(2000);
-    });
+//    $(".main").find("li").each(function () {
+//        $(this).addClass("colorText backgroundElement");
+//        $(this).fadeOut(2000);
+//        $(this).fadeIn(2000);
+//    });
+    
+/* Zadanie 4
+Za pomocą jQuery wykonaj następujące operacje:
+    - Wyszukaj wszystkie linki i ustaw im czerwony kolor za pomocą funkcji css().
+    - Zmodyfikuj kod tak, aby kolor czerwony miały linki tylko z menu.
+    - Dodaj klasę redLinks w pliku style.css (ustaw w niej kolor tekstu na czerwony) 
+    i za pomocą addClass nadaj ją elementom li w menu.
+    - Spraw, aby pierwszy element menu miał większy font niż inne. Stwórz odpowiednią 
+    klasę w pliku style.css. Pamiętaj, aby wykonać to w odpowiedniej funkcji.  */    
+
+//$(".menu a").css("color", "red");
+$(".menu a").addClass("redLinks");
+$(".menu a").first().addClass("fontTest4");
 
 
 
