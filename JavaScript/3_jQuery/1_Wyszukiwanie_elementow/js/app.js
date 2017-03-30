cdf700f7ba8f38bf2cdb1f4a1db2d1634a5f9f62
ccwrc <j.muszelik@gmail.com>
2017-03-30 16:41:56 +0200

    zadanie 2 - done

--- JavaScript/3_jQuery/1_Wyszukiwanie_elementow/js/app.js
+++ JavaScript/3_jQuery/1_Wyszukiwanie_elementow/js/app.js
@@ -1,8 +1,22 @@
 /* Zadanie 1 
-Dołącz do swojego pliku bibliotekę jQuery. W pliku app.js umieść kod sprawdzający, 
-czy DOM został załadowany. */
+ Dołącz do swojego pliku bibliotekę jQuery. W pliku app.js umieść kod sprawdzający, 
+ czy DOM został załadowany. */
 
-$(document).ready(function() {
-    console.log("DOM załadowany");
-    
+$(document).ready(function () {
+//    console.log("DOM załadowany");
+
+/* Zadanie 2 
+Zapoznaj się z plikiem index.html oraz plikiem style.css. Dodaj klasę borderClass do każdego
+ elementu li w elemencie section class="main". Pamiętaj, aby wykonać to w odpowiedniej funkcji.
+  Znajdź wszystkie elementy o klasie showMore. Pierwszemu z tych elementów zmień kolor 
+  fontu css() na niebieski. */
+
+  $(".main li").addClass("borderClass");
+  $(".showMore").first().css("color", "blue");
+
+
+
+
+
+
 });
