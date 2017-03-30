508d8b5889f3667fbf248eaaae9198acde4806c0
ccwrc <j.muszelik@gmail.com>
2017-03-30 16:00:20 +0200

    zadanie 1 - done

---
+++ JavaScript/3_jQuery/1_Wyszukiwanie_elementow/js/app.js
@@ -0,0 +1,8 @@
+/* Zadanie 1 
+Dołącz do swojego pliku bibliotekę jQuery. W pliku app.js umieść kod sprawdzający, 
+czy DOM został załadowany. */
+
+$(document).ready(function() {
+    console.log("DOM załadowany");
+    
+});
