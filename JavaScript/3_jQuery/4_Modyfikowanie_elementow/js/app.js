fc849c51aedb81d7c6c176dfb2220a592b99a6da
ccwrc <j.muszelik@gmail.com>
2017-04-01 16:26:34 +0200

    3_jQuery - done

--- JavaScript/3_jQuery/4_Modyfikowanie_elementow/js/app.js
+++ JavaScript/3_jQuery/4_Modyfikowanie_elementow/js/app.js
@@ -36,8 +36,24 @@ $(document).ready(function () {
         colorThis();
     });
 
+    /* Zadanie 2
+     Znajdź w pliku index.html element o klasie where-i-am, następnie stwórz odpowiednią funkcję, 
+     wewnątrz której stwórz elementy span i dodaj je w odpowiednie miejsca według obrazka poniżej.
+     W miejsce trzech kropek wstaw nazwę funkcji, której używasz, np. Jestem tutaj append. Nie 
+     zmieniaj kodu HTML. */
 
+    function fourElements() {
+        var newSpan1 = $("<span>1.Jestem tutaj append</span>");
+        var newSpan2 = $("<span>2.Jestem tutaj before</span>");
+        var newSpan3 = $("<span>3.Jestem tutaj before</span>");
+        var newSpan4 = $("<span>4.Jestem tutaj after</span>");
 
+        $("section.where-i-am div").append(newSpan1);
+        $("section.where-i-am div p").before(newSpan2);
+        $("section.where-i-am div").before(newSpan3);
+        $("section.where-i-am div").after(newSpan4);
+    }
 
+    fourElements();
 
 });
