2b1ced9b3736126d554cae1c0ea1adedad16501a
ccwrc <j.muszelik@gmail.com>
2017-04-02 21:02:07 +0200

    0_Debuggowanie - done

--- JavaScript/Zadania_dodatkowe_JavaScript/0_Debuggowanie/js/zadanie03.js
+++ JavaScript/Zadania_dodatkowe_JavaScript/0_Debuggowanie/js/zadanie03.js
@@ -1,33 +1,32 @@
-function checkNumber(number){
-  var type = typeof number;
+function checkNumber(number) {
+    var type = typeof number;
 
-  if(type === 'string') {
-   return 'Your number is string';
-  }
+    if (type === 'string') {
+        return 'Your number is string';
+    }
 
-  if(type === undefined) {
-   return 'Your number is undefined';
-  }
+    if (type === 'undefined') {
+        return 'Your number is undefined';
+    }
 
-  if(type === null) {
-   return 'Your number is null';
-  }
+    if (type === 'object') {
+        return 'Your number is null';
+    }
 
-  return 'Your number is really number';
+    return 'Your number is really number';
 
 }
 
 
 function countSomething() {
- var foo;
- console.log(checkNumber(foo));
+    var foo;
+    console.log(checkNumber(foo));
 
- var bar = "lalalala";
- console.log(checkNumber(bar));
+    var bar = "lalalala";
+    console.log(checkNumber(bar));
 
-
- var baz = null;
- console.log(checkNumber(baz));
+    var baz = null;
+    console.log(checkNumber(baz));
 }
 
 countSomething();
