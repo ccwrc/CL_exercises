5f96646f9a52f4fd72a46e7a93282a1df46a7ae2
ccwrc <j.muszelik@gmail.com>
2017-04-02 20:45:29 +0200

    zadanie02 - done

--- JavaScript/Zadania_dodatkowe_JavaScript/0_Debuggowanie/js/zadanie02.js
+++ JavaScript/Zadania_dodatkowe_JavaScript/0_Debuggowanie/js/zadanie02.js
@@ -1,34 +1,38 @@
 function doSomethingElse() {
-  var wheather = "sunny";
-  var things = "";
+    var wheather = "sunny";
+    var things = "";
 
-  switch (wheather){
-    case "sunny": {
-      things = "hat";
+    switch (wheather) {
+        case "sunny":
+        {
+            things = "hat";
+        }
+        case "windy":
+        {
+            things = "jacket";
+        }
+        case "rainy":
+        {
+            things = "umbrella";
+        }
+        case "frostily" :
+        {
+            things = "coat";
+        }
     }
-    case "windy": {
-     things = "jacket";
-    }
-    case "rainy": {
-      things = "umbrella";
-    }
-    case "frostily" : {
-      things = "coat";
-    }
-  }
 
-  var temp = 0;
+    var temp = 0;
 
-  if(temp < -10 ) {
-   var things += ' gloves';
-  } else if ( temp < 0 ) {
-   var things += ' cap';
-  } else if (temp < 10) {
-   var things += ' hat';
-  }
+    if (temp < -10) {
+        things += ' gloves';
+    } else if (temp < 0) {
+        things += ' cap';
+    } else if (temp < 10) {
+        things += ' hat';
+    }
 
-  console.log('temperature is: ' + temp);
-  console.log('what sould I take: ' + things);
+    console.log('temperature is: ' + temp);
+    console.log('what sould I take: ' + things);
 
 }
 
