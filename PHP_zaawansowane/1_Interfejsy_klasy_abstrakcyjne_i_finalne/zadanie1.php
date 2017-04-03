53345ece13837095c44ccc35ec81821611ed873f
ccwrc <j.muszelik@gmail.com>
2017-04-03 17:56:55 +0200

    class admin - done

--- PHP_zaawansowane/1_Interfejsy_klasy_abstrakcyjne_i_finalne/zadanie1.php
+++ PHP_zaawansowane/1_Interfejsy_klasy_abstrakcyjne_i_finalne/zadanie1.php
@@ -27,3 +27,29 @@ abstract class User {
     }
 
 }
+
+
+class Admin extends User {
+
+    public function setLogin($username) {
+        if (strlen($username) >= 10) {
+            $this->username = $username;
+        }
+    }
+
+    public function setPassword($password) {
+        if (strlen($password) >= 10) {
+            $this->password = $password;
+        }
+    }
+
+    protected function checkLogin($username, $password) {
+        if ($this->username === $username && $this->password === $password 
+                && $_SERVER['REMOTE_ADDR'] === '192.168.33.22') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
