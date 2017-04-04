5df14ec1cc1e5e187904d370bd4c72c2d082262b
ccwrc <j.muszelik@gmail.com>
2017-04-04 16:09:07 +0200

    punkt 1,2 - done

---
+++ JavaScript/Zadania_dodatkowe_JavaScript/2_Gallery/js/app.js
@@ -0,0 +1,17 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    var allLi = document.querySelectorAll("div ul li");
+    var body = document.querySelector("body");
+    
+    console.log(allLi);
+    console.log(body);
+    
+});
+
+/*
+ <div class="fullScreen">
+   <img src="./images/wrong.png">
+   <button class="close">Close</button>
+ </div>
+ */
