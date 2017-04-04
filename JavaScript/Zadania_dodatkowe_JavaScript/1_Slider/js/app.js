6d4d5dbc143bbb4138b5704fe79a2c204988e986
ccwrc <j.muszelik@gmail.com>
2017-04-04 15:30:51 +0200

    punkt 3 - done

--- JavaScript/Zadania_dodatkowe_JavaScript/1_Slider/js/app.js
+++ JavaScript/Zadania_dodatkowe_JavaScript/1_Slider/js/app.js
@@ -6,16 +6,14 @@ document.addEventListener("DOMContentLoaded", function() {
     var next = document.querySelector("#nextPicture");
     var prev = document.querySelector("#prevPicture");
     var allLi = document.querySelectorAll("div ul li");
-    var allLiTab = [];
-    var pictureIndex = 0;
+//    var allLiTab = [];
+    var pictureIndex = 1;
     
-    for (var i=0; i<allLi.length; i++) {
-        allLiTab[i] = allLi[i];
-    }
-    console.log(allLi);
-    console.log(allLiTab);
-    console.log(next);
-    console.log(prev);
+//    for (var i=0; i<allLi.length; i++) {
+//        allLiTab[i] = allLi[i];
+//    }
+  
+    allLi[pictureIndex].classList.add("visible");
     
     
     
