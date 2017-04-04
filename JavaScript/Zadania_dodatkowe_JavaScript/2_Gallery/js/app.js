5396181ad8aad1b09cc52f772324d4a0df5f7e9d
ccwrc <j.muszelik@gmail.com>
2017-04-04 17:40:15 +0200

    2_Gallery - done

--- JavaScript/Zadania_dodatkowe_JavaScript/2_Gallery/js/app.js
+++ JavaScript/Zadania_dodatkowe_JavaScript/2_Gallery/js/app.js
@@ -1,17 +1,36 @@
 
-document.addEventListener("DOMContentLoaded", function() {
-    
-    var allLi = document.querySelectorAll("div ul li");
+document.addEventListener("DOMContentLoaded", function () {
+
+    var allLi = document.querySelectorAll("div ul li img");
     var body = document.querySelector("body");
-    
-    console.log(allLi);
-    console.log(body);
-    
+
+    for (var i = 0; i < allLi.length; i++) {
+        allLi[i].addEventListener("click", function () {
+            var source = this.getAttribute("src");
+            var fullScreenDiv = document.createElement("div");
+            var fullScreenImg = document.createElement("img");
+            var closeButton = document.createElement("button");
+
+            closeButton.innerHTML = "Close";
+            fullScreenDiv.classList.add("fullScreen");
+            closeButton.classList.add("close");
+            fullScreenImg.setAttribute("src", source);
+
+            fullScreenDiv.appendChild(fullScreenImg);
+            fullScreenDiv.appendChild(closeButton);
+
+            closeButton.addEventListener("click", function () {
+                body.removeChild(fullScreenDiv);
+            });
+
+            fullScreenImg.addEventListener("click", function () {
+                body.removeChild(fullScreenDiv);
+            }); // po zmniejszeniu okna niewidoczny button close, dodana funkcja do img
+
+            body.appendChild(fullScreenDiv);
+        });
+    }
+
+
 });
 
-/*
- <div class="fullScreen">
-   <img src="./images/wrong.png">
-   <button class="close">Close</button>
- </div>
- */
