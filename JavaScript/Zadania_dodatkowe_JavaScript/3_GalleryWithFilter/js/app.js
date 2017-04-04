cd420142d7c24030fc3b89af6ec29ab2ad06256c
ccwrc <j.muszelik@gmail.com>
2017-04-04 17:52:20 +0200

    punkt 1,2 - done

---
+++ JavaScript/Zadania_dodatkowe_JavaScript/3_GalleryWithFilter/js/app.js
@@ -0,0 +1,16 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    var images = document.querySelectorAll("div#gallery img");
+    var showButton = document.querySelector("#showButton");
+    var hideButton = document.querySelector("#hideButton");
+    var inputTag = document.querySelector("#tagInput");
+    
+    console.log(images);
+    console.log(showButton);
+    console.log(hideButton);
+    console.log(inputTag);
+    
+
+    
+});
