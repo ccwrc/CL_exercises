073ef90c7eb994845df777ff4843d63e4e2c480c
ccwrc <j.muszelik@gmail.com>
2017-04-04 15:39:58 +0200

    punkt 4,5,6,7 - done

--- JavaScript/Zadania_dodatkowe_JavaScript/1_Slider/js/app.js
+++ JavaScript/Zadania_dodatkowe_JavaScript/1_Slider/js/app.js
@@ -1,21 +1,30 @@
 
-document.addEventListener("DOMContentLoaded", function() {
-    
-    console.log("test");
-    
+document.addEventListener("DOMContentLoaded", function () {
+
     var next = document.querySelector("#nextPicture");
     var prev = document.querySelector("#prevPicture");
     var allLi = document.querySelectorAll("div ul li");
 //    var allLiTab = [];
     var pictureIndex = 1;
-    
+
 //    for (var i=0; i<allLi.length; i++) {
 //        allLiTab[i] = allLi[i];
 //    }
-  
+
     allLi[pictureIndex].classList.add("visible");
-    
-    
-    
-    
+
+    next.addEventListener("click", function () {
+        allLi[pictureIndex].classList.remove("visible");
+        pictureIndex++;
+        allLi[pictureIndex].classList.add("visible");
+    });
+
+    prev.addEventListener("click", function () {
+        allLi[pictureIndex].classList.remove("visible");
+        pictureIndex--;
+        allLi[pictureIndex].classList.add("visible");
+    });
+
+
+
 });
