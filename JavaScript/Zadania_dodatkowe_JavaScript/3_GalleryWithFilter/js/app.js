7e34480c7b4e465df96247a07bf2aca009bcf88b
ccwrc <j.muszelik@gmail.com>
2017-04-04 17:57:33 +0200

    punkt 3 - done

--- JavaScript/Zadania_dodatkowe_JavaScript/3_GalleryWithFilter/js/app.js
+++ JavaScript/Zadania_dodatkowe_JavaScript/3_GalleryWithFilter/js/app.js
@@ -1,16 +1,19 @@
 
-document.addEventListener("DOMContentLoaded", function() {
-    
+document.addEventListener("DOMContentLoaded", function () {
+
     var images = document.querySelectorAll("div#gallery img");
     var showButton = document.querySelector("#showButton");
     var hideButton = document.querySelector("#hideButton");
     var inputTag = document.querySelector("#tagInput");
-    
-    console.log(images);
-    console.log(showButton);
-    console.log(hideButton);
-    console.log(inputTag);
-    
 
-    
+    showButton.addEventListener("click", function () {
+        console.log("show");
+    });
+
+    hideButton.addEventListener("click", function () {
+        console.log("hide");
+    });
+
+
+
 });
