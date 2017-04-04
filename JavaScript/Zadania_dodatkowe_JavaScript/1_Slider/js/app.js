9fbbda5dd9f8ead8c26f91cf63ea48c6ab94077f
ccwrc <j.muszelik@gmail.com>
2017-04-04 15:54:02 +0200

    1_Slider - done

--- JavaScript/Zadania_dodatkowe_JavaScript/1_Slider/js/app.js
+++ JavaScript/Zadania_dodatkowe_JavaScript/1_Slider/js/app.js
@@ -4,27 +4,27 @@ document.addEventListener("DOMContentLoaded", function () {
     var next = document.querySelector("#nextPicture");
     var prev = document.querySelector("#prevPicture");
     var allLi = document.querySelectorAll("div ul li");
-//    var allLiTab = [];
-    var pictureIndex = 1;
-
-//    for (var i=0; i<allLi.length; i++) {
-//        allLiTab[i] = allLi[i];
-//    }
+    var pictureIndex = 0;
 
     allLi[pictureIndex].classList.add("visible");
 
     next.addEventListener("click", function () {
         allLi[pictureIndex].classList.remove("visible");
         pictureIndex++;
+        if (pictureIndex >= allLi.length) {
+            pictureIndex = 0;
+        }
         allLi[pictureIndex].classList.add("visible");
     });
 
     prev.addEventListener("click", function () {
         allLi[pictureIndex].classList.remove("visible");
         pictureIndex--;
+        if (pictureIndex < 0) {
+            pictureIndex = allLi.length - 1;
+        }
         allLi[pictureIndex].classList.add("visible");
     });
-
 
 
 });
