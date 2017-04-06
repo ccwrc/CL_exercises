63ab68813491e5fffc743ef5ada851c7d757c3cb
ccwrc <j.muszelik@gmail.com>
2017-04-06 16:59:36 +0200

    2_Tabs - done

--- JavaScript/Zadania_dodatkowe_jQuery/2_Tabs/js/app.js
+++ JavaScript/Zadania_dodatkowe_jQuery/2_Tabs/js/app.js
@@ -1,21 +1,15 @@
 
 $(document).ready(function () {
 
-    /* Punkt 3
-
-Znajdź następujące elementy i zapisz je do zmiennych:
-
-    Wszystkie elementy listy odpowiadające tabom.
-    Wszystkie divy odpowiadające tekstom.
-
-Sprawdź, czy wyszukałeś odpowiednie elementy i czy ich liczba się zgadza.
-
-Wypisz zmienne w konsoli, żeby upewnić się, czy zawierają poprawne dane. */
-    
     var allLi = $("li");
-    console.log(allLi);
-    
     var allDivsWithText = $("div").find("div");
-    console.log(allDivsWithText);
+
+    allLi.each(function (index, element) {
+        $(element).on("click", function () {
+            allDivsWithText.hide();
+            allDivsWithText.eq(index).slideToggle(400);
+        });
+    });
+
 
 });
