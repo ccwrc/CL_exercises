a00a3daf4031e68df1a29a07846fd77dd6cf4519
ccwrc <j.muszelik@gmail.com>
2017-04-06 18:21:47 +0200

    3_StickyMenu - done

--- JavaScript/Zadania_dodatkowe_jQuery/3_StickyMenu/js/app.js
+++ JavaScript/Zadania_dodatkowe_jQuery/3_StickyMenu/js/app.js
@@ -1,12 +1,22 @@
 
 $(document).ready(function () {
 
-var nav = $("nav");
-console.log(nav);
+    var nav = $("nav");
+    var positionMenu = nav.position().top;
 
-var menu = $(".menu").find("ul");
-console.log(menu);
+    $(window).on("scroll", function () {
+        var positionDoc = $(document).scrollTop();
+        if (positionDoc > positionMenu) {
+            nav.addClass("sticky");
+        } else {
+            nav.removeClass("sticky");
+        }
+    });
 
-var position = "top";
+    $(window).on("resize", function () {
+        if (nav.hasClass("sticky")) {
+            positionMenu = nav.position().top;
+        }
+    });
 
 });
