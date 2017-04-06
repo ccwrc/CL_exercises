c57e3cc0d8ee5a9ad2a2345f4c160d47e5b53c04
ccwrc <j.muszelik@gmail.com>
2017-04-06 14:40:44 +0200

    punkt 1,2 - done

---
+++ JavaScript/Zadania_dodatkowe_jQuery/1_QandA/js/app.js
@@ -0,0 +1,8 @@
+/* Napisz prosty schemat pytań i odpowiedzi. Docelowo odpowiedzi mają być niewidoczne. 
+ * Powinny ukazywać się dopiero po kliknięciu na pytanie. */
+
+$(document).ready(function () {
+
+    console.log("dziala");
+
+});
