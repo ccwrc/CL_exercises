bea446c9ca777b30552c81702d77ed9aa597527c
ccwrc <j.muszelik@gmail.com>
2017-04-09 15:55:11 +0200

    Zadania_dodatkowe_jQuery - done

--- JavaScript/Zadania_dodatkowe_jQuery/7_Forms/js/app.js
+++ JavaScript/Zadania_dodatkowe_jQuery/7_Forms/js/app.js
@@ -6,12 +6,28 @@ $(document).ready(function () {
     var inputEmail = $("#emailInput");
     var inputMessage = $("#messageInput");
     var errorDiv = $("div.error");
-    // errorDiv.text("testerror");
 
     form.on("submit", function (event) {
-        event.preventDefault();
-        console.log("klikform");
 
+        var name = inputName.val();
+        var email = inputEmail.val();
+        var message = inputMessage.val();
+
+        if (name.length <= 5) {
+            errorDiv.text("min. 6 znaków w name");
+            event.preventDefault();
+        }
+
+        var regEx = new RegExp(/.*@.*\..*/);
+        if (!regEx.test(email)) {
+            errorDiv.text("błędny email");
+            event.preventDefault();
+        }
+
+        if (message.length <= 10) {
+            errorDiv.text("min. 10 znaków w message");
+            event.preventDefault();
+        }
 
     });
 
