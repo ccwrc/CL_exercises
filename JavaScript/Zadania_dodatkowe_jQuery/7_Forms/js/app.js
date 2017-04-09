3256224b0091d9eed3591f5415722c8eab4d0fa9
ccwrc <j.muszelik@gmail.com>
2017-04-09 14:25:28 +0200

    punkt 2 - done

--- JavaScript/Zadania_dodatkowe_jQuery/7_Forms/js/app.js
+++ JavaScript/Zadania_dodatkowe_jQuery/7_Forms/js/app.js
@@ -1,6 +1,17 @@
 
 $(document).ready(function () {
 
-    console.log("dziala");
+    var form = $("#contactForm");
+    console.log(form);
+    var inputName = $("#nameInput");
+    console.log(inputName);
+    var inputEmail = $("#emailInput");
+    console.log(inputEmail);
+    var inputMessage = $("#messageInput");
+    console.log(inputMessage);
+    var errorDiv = $("div.error");
+    console.log(errorDiv);
+    errorDiv.text("testerror");
+
 
 });
