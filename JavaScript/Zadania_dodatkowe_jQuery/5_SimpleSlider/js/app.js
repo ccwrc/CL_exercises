f5f09e31946ca2ea208f972ae84354d0f2cd3b30
ccwrc <j.muszelik@gmail.com>
2017-04-09 13:05:19 +0200

    5_SimpleSlider - done

--- JavaScript/Zadania_dodatkowe_jQuery/5_SimpleSlider/js/app.js
+++ JavaScript/Zadania_dodatkowe_jQuery/5_SimpleSlider/js/app.js
@@ -4,33 +4,26 @@ $(document).ready(function () {
     var nextButton = $("#nextPicture");
     var prevButton = $("#prevPicture");
     var allLi = $("ul").find("li");
+    var images = $(".slider").find("img");
     var imgIndex = 0;
     var imgWidth = 400;
 
     nextButton.on("click", function () {
         imgIndex++;
-        if (imgIndex >= 6) {
+        if (imgIndex > images.length - 1) {
             imgIndex = 0;
         }
         // minus przy imgIndex powoduje, ze mozna uzyc left zamiast rigth
         $("ul").animate({"left": -imgIndex * imgWidth + "px"}, 500);
     });
 
-    prevButton.on("click", function () { //smiec do poprawy
+    prevButton.on("click", function () {
         imgIndex--;
         if (imgIndex < 0) {
-            imgIndex = 5;
+            imgIndex = images.length - 1;
         }
         $("ul").animate({"left": -imgIndex * imgWidth + "px"}, 500);
     });
 
-    /* Zmodyfikuj event tak, żeby po kliknięciu w przycisk:
-     
-     Zwiększyć zmienną przetrzymującą indeks obrazka.
-     Sprawdzić, czy przypadkiem zmienna nie przekracza liczby wszystkich obrazków.
-     Jeśli wszystko jest w porządku, zaanimuj przesuwanie slajdu (wykorzystaj własność left). 
-     Pamiętaj, że element, który przesuwasz (ul), musi mieć ustawione w CSS position: relative. 
-     Zastanów się, w którą stronę chcesz się przesuwać.
-     */
 
 });
