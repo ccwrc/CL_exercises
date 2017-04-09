778a975f05729aa8952d72d17768150a54cb1b7f
ccwrc <j.muszelik@gmail.com>
2017-04-09 13:29:26 +0200

    6_InfiniteSlider - done

---
+++ JavaScript/Zadania_dodatkowe_jQuery/6_InfiniteSlider/js/app.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function () {
+
+    var nextButton = $("#nextPicture");
+    var prevButton = $("#prevPicture");
+    var allLi = $("ul").find("li");
+    var images = $(".slider").find("img");
+    var imgIndex = 0;
+    var imgWidth = 400;
+
+    nextButton.on("click", function () {
+        imgIndex++;
+        if (imgIndex > images.length - 1) {
+            imgIndex = 0;
+        }
+        // minus przy imgIndex powoduje, ze mozna uzyc left zamiast rigth
+        $("ul").animate({"left": -imgIndex * imgWidth + "px"}, 500);
+    });
+
+    prevButton.on("click", function () {
+        imgIndex--;
+        if (imgIndex < 0) {
+            imgIndex = images.length - 1;
+        }
+        $("ul").animate({"left": -imgIndex * imgWidth + "px"}, 500);
+    });
+
+
+});
