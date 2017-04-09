ec5925957b8e9f3de49fd7b7b26869787ce7b0c2
ccwrc <j.muszelik@gmail.com>
2017-04-09 14:31:34 +0200

    punkt 3 - done

--- JavaScript/Zadania_dodatkowe_jQuery/7_Forms/js/app.js
+++ JavaScript/Zadania_dodatkowe_jQuery/7_Forms/js/app.js
@@ -2,16 +2,18 @@
 $(document).ready(function () {
 
     var form = $("#contactForm");
-    console.log(form);
     var inputName = $("#nameInput");
-    console.log(inputName);
     var inputEmail = $("#emailInput");
-    console.log(inputEmail);
     var inputMessage = $("#messageInput");
-    console.log(inputMessage);
     var errorDiv = $("div.error");
-    console.log(errorDiv);
-    errorDiv.text("testerror");
+    // errorDiv.text("testerror");
+
+    form.on("submit", function (event) {
+        event.preventDefault();
+        console.log("klikform");
+
+
+    });
 
 
 });
