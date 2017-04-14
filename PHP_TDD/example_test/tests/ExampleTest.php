a76747a7b5f4d5863a5ca5d238b9c970be805621
ccwrc <j.muszelik@gmail.com>
2017-04-14 18:28:54 +0200

    example_test - done

--- PHP_TDD/example_test/tests/ExampleTest.php
+++ PHP_TDD/example_test/tests/ExampleTest.php
@@ -16,5 +16,53 @@ class ExampleTest extends PHPUnit_Framework_TestCase {
         $value = 10.99;
         $this->assertTrue(is_numeric($value), 'value oblalo test');
     }
+    
+    public function testIsExpectedIsEqual() { // gdy typy musza sie zgadzac: assertSame
+        $expected = '10';
+        $this->assertEquals($expected, 5 + 5, 'oblany test expected=10');
+    }
+
+    public function testIsFruitInArray() {
+        $fruits = ['Apple', 'Orange', 'Grapefruit'];
+        $this->assertContains('Apple', $fruits, "brak owoca w tablicy");
+    }// jesli szukamy klucza: assertArrayHasKey
+    
+    // czy podany parametr (jako drugi) jest konkretnego typu. Typ musi być wyrażony jako string
+    public function testIsSecondParameterIsArray() {
+        $fruits = ['Apple', 'Orange', 'Grapefruit'];
+        $this->assertInternalType('array', $fruits);
+    } // http://php.net/manual/en/language.types.php
+
+    public function testIsSecondParameterIsBool() {
+        $bool = false;
+        $this->assertInternalType('bool', $bool);
+    }
+
+    public function testIsSecondParameterIsInt() {
+        $param = 11;
+        $this->assertInternalType('int', $param); // integer tez dziala
+    }
+
+    public function testIsSecondParameterIsString() {
+        $param = "11";
+        $this->assertInternalType('string', $param, "tu powinien byc string");
+    }
+    
+    public function testIsDateIsObject() {
+        $date = new DateTime();
+        // $date = "new DateTime()";
+        $this->assertInstanceOf('DateTime', $date);
+    }
+
+    // czy dany test rzuca wyjatek    
+    /**
+     * @expectedException Exception
+     */
+    public function testMustThrowException() {
+        throw new Exception("Error", 1);
+    } // i jak rzuca to zalicza
+    // inne do adnotacji
+    // @expectedExceptionCode, @expectedExceptionMessage oraz @expectedExceptionMessageRegE
+    
 
 }
