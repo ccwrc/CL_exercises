2091d7822d564329fc7d284a14cc200290d68126
ccwrc <j.muszelik@gmail.com>
2017-04-15 18:38:05 +0200

    a3 add function leapYear

---
+++ PHP_TDD/A3/leapYear.php
@@ -0,0 +1,17 @@
+<?php
+
+/* Napisz funkcję, która przyjmuje liczbę całkowitą (oznaczającą rok) i zwraca true – jeżeli rok
+ *  jest przestępny, false jeżeli nie. Napisz testy do tej funkcji. */
+
+function leapYear($year) {
+
+    if ($year <= 0 || !is_int($year)) {
+        throw new Exception('nieprawidłowa wartość');
+    }
+
+    if ((($year % 4) == 0) && ((($year % 100) != 0) || (($year % 400) == 0))) {
+        return true;
+    } else {
+        return false;
+    }
+}
