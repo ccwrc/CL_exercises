86ebaabbe9388f74ef6bffe06bdb1e6b5447444e
ccwrc <j.muszelik@gmail.com>
2017-04-16 14:44:46 +0200

    A4 tests - done

---
+++ PHP_TDD/A4/tests/WordWrapTest.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once __DIR__ . '/../wordWrap.php';
+
+class WordWrapTest extends PHPUnit_Framework_TestCase {
+    
+    public function testIfReturnFalse() {
+        $this->assertFalse(newWordWrap("string", "string"));
+        $this->assertFalse(newWordWrap(22, 33));
+    }
+    
+    public function testIfReturnCorrectType() {
+        $this->assertInternalType('string', newWordWrap("string 22", 3));
+        $this->assertInternalType('bool', newWordWrap("string 22", 3.11));
+    }
+    
+    public function stringProvider(){
+        return [
+            ["GT WWW, uuu, zz", 2, "GT..."],
+            ["zwykłe lorem ipsum", 10, "zwykłe..."],
+            ["zwykłe lorem ipsum", 9, "zwykłe..."],
+            ["zwykłe lorem ipsum", 900, "zwykłe lorem ipsum"],
+            ["zwykłe lorem ipsum", 0, "..."],
+            ["zwykłe lorem ipsum", 2, "..."]
+        ];
+    }
+    
+    /**
+     * @dataProvider stringProvider
+     */
+    public function testIsReturnCorrectString($string, $length, $expected) {
+        $this->assertEquals($expected, newWordWrap($string, $length));
+    }
+    
+     /**
+     * @expectedException Exception
+     */
+    public function testIfThrowsExceptionForForgottenValue() {
+        newWordWrap("string");
+        newWordWrap(22);
+        newWordWrap();        
+    }
+  
+}
