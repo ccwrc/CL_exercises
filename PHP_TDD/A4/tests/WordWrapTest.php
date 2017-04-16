b4fec304b96963e7391263667755b7cdca6c4301
ccwrc <j.muszelik@gmail.com>
2017-04-16 15:24:29 +0200

    A4 - done

--- PHP_TDD/A4/tests/WordWrapTest.php
+++ PHP_TDD/A4/tests/WordWrapTest.php
@@ -3,20 +3,20 @@
 require_once __DIR__ . '/../wordWrap.php';
 
 class WordWrapTest extends PHPUnit_Framework_TestCase {
-    
+
     public function testIfReturnFalse() {
         $this->assertFalse(newWordWrap("string", "string"));
         $this->assertFalse(newWordWrap(22, 33));
     }
-    
+
     public function testIfReturnCorrectType() {
         $this->assertInternalType('string', newWordWrap("string 22", 3));
         $this->assertInternalType('bool', newWordWrap("string 22", 3.11));
     }
-    
-    public function stringProvider(){
+
+    public function stringProvider() {
         return [
-            ["GT WWW, uuu, zz", 2, "GT..."],
+            ["GT WWW, uuu, zz", 3, "GT..."],
             ["zwykłe lorem ipsum", 10, "zwykłe..."],
             ["zwykłe lorem ipsum", 9, "zwykłe..."],
             ["zwykłe lorem ipsum", 900, "zwykłe lorem ipsum"],
@@ -24,21 +24,21 @@ class WordWrapTest extends PHPUnit_Framework_TestCase {
             ["zwykłe lorem ipsum", 2, "..."]
         ];
     }
-    
+
     /**
      * @dataProvider stringProvider
      */
     public function testIsReturnCorrectString($string, $length, $expected) {
         $this->assertEquals($expected, newWordWrap($string, $length));
     }
-    
-     /**
-     * @expectedException Exception
+
+    /**
+     * @expectedException PHPUnit_Framework_Error_Warning
      */
     public function testIfThrowsExceptionForForgottenValue() {
+        newWordWrap();
         newWordWrap("string");
         newWordWrap(22);
-        newWordWrap();        
     }
-  
+
 }
--- PHP_TDD/A4/wordWrap.php
+++ PHP_TDD/A4/wordWrap.php
@@ -1,36 +1,27 @@
 <?php
+
 /* Napisz funkcję wordWrap($string, $length), której zadaniem jest skrócić napis do podanej
  *  długości (i dodanie na końcu ...). Funkcja ma działać w taki sposób żeby, żadne słowo 
  * nigdy nie zostało przecięte. Napisz testy do tej funkcji. */
 
-function newWordWrap ($string, $length) { //php posiada juz wbudowana funkcje wordwrap
-    
+function newWordWrap($string, $length) { //php posiada juz wbudowana funkcje wordwrap
     if (!is_string($string) || !is_int($length)) {
         return false;
     }
-    
-    $totalLength = strlen($string);
-    
-    if ($length < $totalLength) {
-//        for ($i = $length-1; $i <= $totalLength-1; $i--) {
-//            if ($string[$i] == " ") {
-//                $newString = substr($string, 0, $i);
-//                break;
-//            } 
-//        }
-        
-        $newString .= "...";
-        return $newString;
+
+    if ($length < strlen($string)) {
+        for ($i = $length - 1; $i >= 0; $i--) {
+            if ($string[$i] == " ") {
+                $newString = substr($string, 0, $i);
+                return $newString .= "...";
+            }
+        }
+        return "...";
     } else {
         return $string;
     }
-    
 }
 
-
-$string = "bl aecar ble bue  baaa";
-var_dump(newWordWrap("yy"));
-//
 //// http://php.net/manual/en/function.wordwrap.php
 //var_dump(wordWrap($string, 9, "..."));
 
