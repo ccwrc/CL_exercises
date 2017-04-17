66d4fc52630b61d5fc19a01c512760195243122e
ccwrc <j.muszelik@gmail.com>
2017-04-17 13:43:29 +0200

    B1 - done

--- PHP_TDD/B1/src/BowlingGame.php
+++ PHP_TDD/B1/src/BowlingGame.php
@@ -1,45 +1,99 @@
 <?php
+
 /* Napisz klasę o nazwie BowlingGame służącą do podliczania wyników w grze w kręgle. Klasa ma mieć
  *  co najmniej dwie funkcje:
 
-    roll(pins) – funkcja używana, gdy gracz rzuca,
-    score() – funkcja zwracająca liczbę punktów uzyskanych do tej pory.
+  roll(pins) – funkcja używana, gdy gracz rzuca,
+  score() – funkcja zwracająca liczbę punktów uzyskanych do tej pory.
 
-Zasady, które muszą być spełnione:
+  Zasady, które muszą być spełnione:
 
-    Gra w kręgle składa się z 10 ramek.
-    Każda ramka składa się z dwóch rzutów, w których można uzyskać maksymalnie 10 punktów (tyle jest
+  Gra w kręgle składa się z 10 ramek.
+  Każda ramka składa się z dwóch rzutów, w których można uzyskać maksymalnie 10 punktów (tyle jest
  *  kręgli na torze).
-    Liczba punktów z ramki to liczba zbitych kręgli + bonusy za strike i spare.
-    Spare – gracz zbija wszystkie 10 kręgli w dwóch rzutach należących do jednej ramki. Wtedy do 
+  Liczba punktów z ramki to liczba zbitych kręgli + bonusy za strike i spare.
+  Spare – gracz zbija wszystkie 10 kręgli w dwóch rzutach należących do jednej ramki. Wtedy do
  * punktów z tej ramki są doliczane punkty z następnego rzutu (pierwszego rzutu następnej ramki).
  *  Np: W pierwszej ramce gracz rzucił 6 i 4. W kolejnej ramce rzucił 3 i 5. Punktacja za
  *  pierwszą ramkę to 13 (6 + 4 + 3), punktacja za drugą ramkę to 8.
-    Strike – gracz zbija wszystkie kręgle za pierwszym rzutem należącym do ramki (nie ma drugiego 
+  Strike – gracz zbija wszystkie kręgle za pierwszym rzutem należącym do ramki (nie ma drugiego
  * rzutu w tej ramce). W takim przypadku do wyniku z tej ramki dodawane są punkty z kolejnych 
  * dwóch rzutów. Np: W pierwszej ramce gracz rzucił 10. W kolejnej – 3 i 5. Punktacja za pierwszą 
  * ramkę to 17 (10 + 3 + 5), punktacja za drugą ramkę to 8.
-    Jeżeli w ostatniej ramce gracz rzucił strike albo spare, to przysługują mu dodatkowe 
+  Jeżeli w ostatniej ramce gracz rzucił strike albo spare, to przysługują mu dodatkowe
  * rzuty (ale nie więcej niż trzy).
 
-Tu upewnisz się, jak powinien działać taki program:
- http://www.bowlinggenius.com/
+  Tu upewnisz się, jak powinien działać taki program:
+  http://www.bowlinggenius.com/
 
-Przed napisaniem klasy stwórz następujące przypadki testowe (ang. test case):
+  Przed napisaniem klasy stwórz następujące przypadki testowe (ang. test case):
 
-    Wszystkie rzuty zbiły 0 kręgli – suma punktów powinna być 0.
-    Wszystkie rzuty zbijają po jednym kręglu – suma punktów powinna być 10.
-    Strike w pierwszym rzucie, następnie 0 zbitych, a potem wszystkie pudła – suma punktów powinna
+  Wszystkie rzuty zbiły 0 kręgli – suma punktów powinna być 0.
+  Wszystkie rzuty zbijają po jednym kręglu – suma punktów powinna być 10.
+  Strike w pierwszym rzucie, następnie 0 zbitych, a potem wszystkie pudła – suma punktów powinna
  *  być 16.
-    Strike w pierwszym rzucie, następnie 3 zbite kręgle, następnie 4 zbite kręgle, a potem wszystkie 
+  Strike w pierwszym rzucie, następnie 3 zbite kręgle, następnie 4 zbite kręgle, a potem wszystkie
  * pudła – suma punktów powinna wynosić 24.
-    Spare w pierwszej ramce (obojętnie jak zdobyty), potem w następnym rzucie 4, następnie 3 i 
+  Spare w pierwszej ramce (obojętnie jak zdobyty), potem w następnym rzucie 4, następnie 3 i
  * reszta same pudła. Suma punktów powinna wynosić 21.
-    Gra perfekcyjna (10 razy strike).
+  Gra perfekcyjna (10 razy strike).
  */
 
 class BowlingGame {
 
+    private $scoreArray;
+    private $points;
+    private $spareSum;
+    private $isPerfect;
 
+    public function __construct() {
+        $this->scoreArray = [];
+        $this->points = 0;
+        $this->spareSum = 0;
+        $this->isPerfect = true;
+    }
+
+    public function roll($pins) {
+        if (is_int($pins) && $pins >= 0 && $pins <= 10) {
+            $this->scoreArray[] = $pins;
+        }
+    }
+
+    public function score() {
+        // kregle -> bowling ninepins ten-pin bowling //w zalez od rodzaju
+        foreach ($this->scoreArray as $throw => $pins) {
+
+            if ($this->isPerfect && $pins < 10) {
+                $this->isPerfect = false;
+            }
+
+            if (!$this->isPerfect || ($this->isPerfect && $throw < 9)) {
+                $this->points += $pins;
+            }
+
+            if ($throw % 2 == 0) {
+                $this->spareSum = 0;
+            }
+
+            $this->spareSum += $pins;
+
+            if ($pins == 10) {
+                //strike
+                if (isset($this->scoreArray[$throw + 1])) {
+                    $this->points += $this->scoreArray[$throw + 1];
+                }
+                if (isset($this->scoreArray[$throw + 2])) {
+                    $this->points += $this->scoreArray[$throw + 2];
+                }
+            } elseif ($this->spareSum == 10) {
+                //spare
+                if (isset($this->scoreArray[$throw + 1])) {
+                    $this->points += $this->scoreArray[$throw + 1];
+                }
+            }
+        }
+
+        return $this->points;
+    }
 
 }
--- PHP_TDD/B1/tests/BowlingGameTest.php
+++ PHP_TDD/B1/tests/BowlingGameTest.php
@@ -3,91 +3,98 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 class BowlingGameTest extends PHPUnit_Framework_TestCase {
-
     protected $bowlingGame;
-    //protected $backupGlobals = ['globalVariable'];
-    
+    // protected $backupGlobals = ['globalVariable'];
+
     protected function setUp() {
+        // https://phpunit.de/manual/current/en/fixtures.html
         $this->bowlingGame = new BowlingGame();
         $_POST['gamerId'] = 4;
         $_GET['isActive'] = true;
     }
-    
+
     protected function tearDown() {
+        // czyszczenie obiektow po testach
         $this->bowlingGame = null;
+        unset($_POST['gamerId']);
+        unset($_GET['isActive']);
     }
-    
+
     public function testIfIhadNoLuckAtAll() {
-        //$this->markTestSkipped('testIfIhadNoLuckAtAll skipped');
-        for($i = 0; $i < 20; $i++) {
+        // $this->markTestSkipped('testIfIhadNoLuckAtAll pominięty');
+        // OK, but incomplete, skipped, or risky tests!
+        for ($i = 0; $i < 20; $i++) {
             $this->bowlingGame->roll(0);
-        }
+        } // 0 zbitych kregli = 0 punktow
         $this->assertEquals(0, $this->bowlingGame->score());
     }
-    
+
     public function testIfIhadAlittleLuck() {
-        //$this->markTestIncomplete('test incomplete');
-        for($i = 0; $i < 10; $i++) {
+        // $this->markTestIncomplete('test niekompletny');
+        // OK, but incomplete, skipped, or risky tests!
+        for ($i = 0; $i < 10; $i++) {
             $this->bowlingGame->roll(1);
             $this->bowlingGame->roll(0);
         }
         $this->assertEquals(10, $this->bowlingGame->score());
     }
-    
+
     public function testIfIhadOneStrike() {
         $this->bowlingGame->roll(10); //strike
-        
         $this->bowlingGame->roll(3);
-        for($i = 0; $i < 17; $i++) {
+        for ($i = 0; $i < 17; $i++) {
             $this->bowlingGame->roll(0);
-        }
+        } // 10 + 3 + 3 bonusu za strike
         $this->assertEquals(16, $this->bowlingGame->score());
     }
-    
+
     public function testIfIHadOneStrikeAndAlittleLuck() {
         $this->bowlingGame->roll(10); //strike
-        
         $this->bowlingGame->roll(3);
         $this->bowlingGame->roll(4);
-        
-        for($i = 0; $i < 16; $i++) {
+
+        for ($i = 0; $i < 16; $i++) {
             $this->bowlingGame->roll(0);
-        }
+        } // bonus za strike 3+4
         $this->assertEquals(24, $this->bowlingGame->score());
     }
-    
+
     public function testIfIHadOneSpare() {
         $this->bowlingGame->roll(3);
-        $this->bowlingGame->roll(7);
-        
-        $this->bowlingGame->roll(4);
+        $this->bowlingGame->roll(7); // 3+7 = spare
+        $this->bowlingGame->roll(4); // bonus za spare - 4 (1 rzut z nast. ramki)
         $this->bowlingGame->roll(3);
-        
-        for($i = 0; $i < 16; $i++) {
+
+        for ($i = 0; $i < 16; $i++) {
             $this->bowlingGame->roll(0);
         }
         $this->assertEquals(21, $this->bowlingGame->score());
     }
-    
+
     public function testIfIHadAperfectGame() {
-        for($i = 0; $i < 12; $i++) {
+        for ($i = 0; $i < 12; $i++) {
             $this->bowlingGame->roll(10);
         }
         $this->assertEquals(300, $this->bowlingGame->score());
     }
-    
+
     public function testIfGamerIdIsCorrect() {
+        // dane z setUp() czyszczone po zakonczeniu testu
         $this->assertEquals(4, $_POST['gamerId']);
     }
-    
+
     public function testIfGamerIsActive() {
+        // dane z setUp() czyszczone po zakonczeniu testu
         $this->assertTrue($_GET['isActive']);
     }
-    
+
     public function testIfConfigurationWorks() {
-        // dane wpisane w konfigu phpunit.xml
+        // dane wpisane w phpunit.xml - test ok
         $this->assertEquals('simple', $GLOBALS['simpleVar']);
         $this->assertEquals(33, $_POST['testVar']);
+        $this->assertEquals(77, $_GET['testVarGet']);
+        // dane z setUp() 
+        $this->assertEquals(4, $_POST['gamerId']);
     }
-    
+
 }
