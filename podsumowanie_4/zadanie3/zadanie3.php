9e0e56387266d68929d92b62889fcd547b53eb27
ccwrc <j.muszelik@gmail.com>
2017-04-23 14:25:23 +0200

    e3 - add phpunit

---
+++ podsumowanie_4/zadanie3/zadanie3.php
@@ -0,0 +1,15 @@
+<?php
+/* Zadanie 3
+W folderze src możesz znaleźć klasę Product. Napisz do niej następujące testy jednostkowe:
+
+    1.Test do tworzenia produktu. Ma on tworzyć nowy produkt i sprawdzać, czy wszystkie 
+ * gettery działają poprawnie.
+    2.Test sprzedaży i kupna produktu. Ma on tworzyć nowy produkt, następnie sprzedać jakąś 
+ * jego ilość i sprawdzić, czy dane się zgadzają. Następnie kupić jakąś jego ilość i ponownie 
+ * sprawdzić, czy dane się zgadzają.
+    3.Test sprawdzania ceny za ilość. Ma on stworzyć nowy produkt i sprawdzić, czy cena 
+ * za podaną ilość jest prawidłowa.
+    4.Test, który sprawdzi działanie metody sprawdzającej, czy produkt jest na stanie.
+
+W klasie Produkt oczywiście są błędy – napraw je tak, żeby wszystkie Twoje testy przechodziły
+ na zielono.  */
